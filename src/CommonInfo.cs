682802ba8cc8f73f74c6c4bf6ae4ca39342b6002
Toby Henderson <hendersont@gmail.com>
2014-06-26 12:41:58 +0100

    Messing around with versioning

--- src/CommonInfo.cs
+++ src/CommonInfo.cs
@@ -4,7 +4,7 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("2.5.0.0")]
-[assembly: AssemblyFileVersion("2.5.0.0")]
+[assembly: AssemblyInformationalVersion("2.5.0")]
 [assembly: AssemblyCompany("Caffeine IT- naughtyProd Limited ")]
 [assembly: AssemblyProduct("OpenRasta 2.5")]
 [assembly: AssemblyCopyright("Copyright (c) Caffeine IT - naughtyProd Limited - Sebastien Lambla ")]
