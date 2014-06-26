59082a0ba804ab587492530778f534f6f70026d7
Toby Henderson <toby.henderson@huddle.com>
2014-06-26 16:35:33 +0100

    Hopefully final version change

--- src/CommonInfo.cs
+++ src/CommonInfo.cs
@@ -1,15 +1,12 @@
 ﻿using System;
 using System.Reflection;
-using System.Resources;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("2.5.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
 [assembly: AssemblyFileVersion("2.5.0.0")]
+[assembly: AssemblyInformationalVersion("2.5.0")]
 [assembly: AssemblyCompany("Caffeine IT- naughtyProd Limited ")]
-[assembly: AssemblyProduct("OpenRasta 2.0 Release Candidate")]
+[assembly: AssemblyProduct("OpenRasta 2.5")]
 [assembly: AssemblyCopyright("Copyright (c) Caffeine IT - naughtyProd Limited - Sebastien Lambla ")]
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(false)]
-
-
