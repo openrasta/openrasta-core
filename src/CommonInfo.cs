172e491e1c786737623461c2d4c3109717bcf384
Sebastien Lambla <seb@serialseb.com>
2016-08-12 00:05:01 +0100

    2.6 update

--- src/CommonInfo.cs
+++ src/CommonInfo.cs
@@ -4,8 +4,8 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.5.0.0")]
-[assembly: AssemblyInformationalVersion("2.5.0")]
+[assembly: AssemblyFileVersion("2.6.0.0")]
+[assembly: AssemblyInformationalVersion("2.6.0-beta1")]
 [assembly: AssemblyCompany("Caffeine IT- naughtyProd Limited ")]
 [assembly: AssemblyProduct("OpenRasta 2.5")]
 [assembly: AssemblyCopyright("Copyright (c) Caffeine IT - naughtyProd Limited - Sebastien Lambla ")]
