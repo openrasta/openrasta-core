a1bbbfcf3789ec85cb0803ac97cbbb037793a226
Sebastien Lambla <seb@serialseb.com>
2016-08-11 14:54:45 +0100

    Adding prerelease

--- src/CommonInfo.cs
+++ src/CommonInfo.cs
@@ -3,8 +3,8 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.6.0.0")]
-[assembly: AssemblyInformationalVersion("2.6.0")]
+[assembly: AssemblyFileVersion("2.6.0")]
+[assembly: AssemblyInformationalVersion("2.6.0-beta1")]
 [assembly: AssemblyCompany("Caffeine IT- naughtyProd Limited ")]
 [assembly: AssemblyProduct("OpenRasta 2.6")]
 [assembly: AssemblyTrademark("OpenRasta is a registered trademark of naughtyProd limited.")]
