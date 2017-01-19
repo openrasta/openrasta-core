2347506f96dca807448406222c040b404ac45f80
Sebastien Lambla <seb@serialseb.com>
2017-01-19 16:24:54 +0000

    Make nunit happy

--- src/OpenRasta.Hosting.AspNet.Tests.Integration/FileCopySetup.cs
+++ src/OpenRasta.Hosting.AspNet.Tests.Integration/FileCopySetup.cs
@@ -1,13 +1,4 @@
-﻿#region License
-/* Authors:
- *      Sebastien Lambla (seb@serialseb.com)
- * Copyright:
- *      (C) 2007-2009 Caffeine IT & naughtyProd Ltd (http://www.caffeine-it.com)
- * License:
- *      This file is distributed under the terms of the MIT License found at the end of this file.
- */
-#endregion
-using System;
+﻿using System;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
@@ -20,17 +11,15 @@ using OpenRasta.IO;
 public class FileCopySetup
 {
     public static DirectoryInfo TempFolder;
-    [SetUp]
-    public void CopyFiles()
+    static FileCopySetup()
     {
         TempFolder = PrepareFolderStructure();
-        //Debugger.Launch();
     }
 
     static DirectoryInfo PrepareFolderStructure()
     {
         var assembly = typeof(aspnet_server_context).Assembly;
-        string localPath = new Uri(assembly.CodeBase).LocalPath;
+        var localPath = new Uri(assembly.CodeBase).LocalPath;
         var rootFolder = Path.GetDirectoryName(localPath);
 
         var tempFolder = CreateTempFolder();
@@ -57,7 +46,7 @@ public class FileCopySetup
     {
         var tempFolder = Path.GetTempPath();
         string createdFolder;
-        int count = 0;
+        var count = 0;
         do
         {
             createdFolder = Path.Combine(tempFolder, "_ORTEST_" + count++);
@@ -88,31 +77,9 @@ public class FileCopySetup
                 });
 
             TempFolder.Delete(true);
-        }catch
+        }
+        catch
         {
         }
     }
 }
-
-#region Full license
-//
-// Permission is hereby granted, free of charge, to any person obtaining
-// a copy of this software and associated documentation files (the
-// "Software"), to deal in the Software without restriction, including
-// without limitation the rights to use, copy, modify, merge, publish,
-// distribute, sublicense, and/or sell copies of the Software, and to
-// permit persons to whom the Software is furnished to do so, subject to
-// the following conditions:
-// 
-// The above copyright notice and this permission notice shall be
-// included in all copies or substantial portions of the Software.
-// 
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-//
-#endregion
