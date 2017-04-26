a441e8287748a7782c5d807025c0c1826350ce46
Sebastien Lambla <seb@serialseb.com>
2017-04-26 18:26:54 +0200

    Refactored in utility methods

--- src/OpenRasta.Hosting.AspNet.Tests.Integration/AsyncYielding.cs
+++ src/OpenRasta.Hosting.AspNet.Tests.Integration/AsyncYielding.cs
@@ -24,46 +24,34 @@ namespace OpenRasta.Hosting.AspNet.Tests.Integration
     [Test]
     public async Task middleware_yielding_same_thread()
     {
-      var pipeline = new IPipelineMiddlewareFactory[]
-      {
+      var didItYield = await InvokeTillYield(
         new YieldingMiddleware(nameof(YieldingMiddleware)),
-        new CodeMiddleware(() => Resumed = true)
-      }.Compose();
+        new CodeMiddleware(() => Resumed = true));
 
-      var operation = pipeline.Invoke(Env);
-
-      var didIt = await Yielding.DidItYield(operation, Env.Yielder(nameof(YieldingMiddleware)).Task);
-
-      didIt.ShouldBeTrue();
+      didItYield.ShouldBeTrue();
       Resumed.ShouldBeFalse();
 
-      Env.Resumer(nameof(YieldingMiddleware)).SetResult(true);
-      await operation;
+      await Resume();
 
-      Resumed.ShouldBeTrue(); 
+      Resumed.ShouldBeTrue();
     }
 
-    [Test]
-    public async Task yielding_same_thread()
+    async Task Resume()
     {
-      var yielded = new TaskCompletionSource<bool>();
-      var resumer = new TaskCompletionSource<bool>();
-
-      var resumed = false;
-      var pipeline = yieldingCode(
-        () => yielded.SetResult(true),
-        () => resumer.Task,
-        () => code(() => resumed = true));
-      var didIt = await Yielding.DidItYield(pipeline, yielded.Task);
-
-      didIt.ShouldBeTrue();
-      resumed.ShouldBeFalse();
-
-      resumer.SetResult(true);
-      await pipeline;
-
-      resumed.ShouldBeTrue();
+      Env.Resumer(nameof(YieldingMiddleware)).SetResult(true);
+      await Operation;
     }
+
+    async Task<bool> InvokeTillYield(params IPipelineMiddlewareFactory[] factories)
+    {
+      Operation = factories.Compose().Invoke(Env);
+
+      return await Yielding.DidItYield(
+        Operation,
+        Env.Yielder(nameof(YieldingMiddleware)).Task);
+    }
+
+    public Task Operation { get; set; }
 
     [Test]
     public async Task yielding_other_thread()
