962e8edd412565a4585612a4666fafccf0b1fcec
Sebastien Lambla <seb@serialseb.com>
2017-04-26 18:33:40 +0200

    Converted test

--- src/OpenRasta.Hosting.AspNet.Tests.Integration/AsyncYielding.cs
+++ src/OpenRasta.Hosting.AspNet.Tests.Integration/AsyncYielding.cs
@@ -10,17 +10,6 @@ namespace OpenRasta.Hosting.AspNet.Tests.Integration
 {
   public class AsyncYielding
   {
-    ICommunicationContext Env;
-
-    [SetUp]
-    public void set_environment()
-    {
-      Env = new InMemoryCommunicationContext();
-      Resumed = false;
-    }
-
-    public bool Resumed { get; set; }
-
     [Test]
     public async Task middleware_yielding_same_thread()
     {
@@ -36,44 +25,22 @@ namespace OpenRasta.Hosting.AspNet.Tests.Integration
       Resumed.ShouldBeTrue();
     }
 
-    async Task Resume()
-    {
-      Env.Resumer(nameof(YieldingMiddleware)).SetResult(true);
-      await Operation;
-    }
-
-    async Task<bool> InvokeTillYield(params IPipelineMiddlewareFactory[] factories)
-    {
-      Operation = factories.Compose().Invoke(Env);
-
-      return await Yielding.DidItYield(
-        Operation,
-        Env.Yielder(nameof(YieldingMiddleware)).Task);
-    }
-
-    public Task Operation { get; set; }
-
     [Test]
-    public async Task yielding_other_thread()
+    public async Task middleware_yielding_other_thread()
     {
-      var yielded = new TaskCompletionSource<bool>();
-      var resumer = new TaskCompletionSource<bool>();
+      var didItYield = await InvokeTillYield(
+        new OtherThreadMiddleware(),
+        new YieldingMiddleware(nameof(YieldingMiddleware)),
+        new CodeMiddleware(()=>Resumed = true)
+        );
 
-      bool resumed = false;
-      var pipeline = Task.Run(
-        () => yieldingCode(
-          () => yielded.SetResult(true),
-          () => resumer.Task,
-          () => code(() => resumed = true)));
-      var didIt = await Yielding.DidItYield(pipeline, yielded.Task);
 
-      didIt.ShouldBeTrue();
-      resumed.ShouldBeFalse();
+      didItYield.ShouldBeTrue();
+      Resumed.ShouldBeFalse();
 
-      resumer.SetResult(true);
-      await pipeline;
+      await Resume();
 
-      resumed.ShouldBeTrue();
+      Resumed.ShouldBeTrue();
     }
 
     [Test]
@@ -166,6 +133,34 @@ namespace OpenRasta.Hosting.AspNet.Tests.Integration
       onCode?.Invoke();
       return Task.FromResult(true);
     }
+
+
+    ICommunicationContext Env;
+
+    [SetUp]
+    public void set_environment()
+    {
+      Env = new InMemoryCommunicationContext();
+      Resumed = false;
+    }
+
+    public bool Resumed { get; set; }
+    async Task Resume()
+    {
+      Env.Resumer(nameof(YieldingMiddleware)).SetResult(true);
+      await Operation;
+    }
+
+    async Task<bool> InvokeTillYield(params IPipelineMiddlewareFactory[] factories)
+    {
+      Operation = factories.Compose().Invoke(Env);
+
+      return await Yielding.DidItYield(
+        Operation,
+        Env.Yielder(nameof(YieldingMiddleware)).Task);
+    }
+
+    public Task Operation { get; set; }
   }
 
   class BypassingCodeMiddleware : IPipelineMiddleware, IPipelineMiddlewareFactory
@@ -181,6 +176,21 @@ namespace OpenRasta.Hosting.AspNet.Tests.Integration
     }
   }
 
+  class OtherThreadMiddleware : IPipelineMiddleware, IPipelineMiddlewareFactory
+  {
+    public Task Invoke(ICommunicationContext env)
+    {
+      return Task.Run(()=> Next.Invoke(env));
+    }
+
+    public IPipelineMiddleware Compose(IPipelineMiddleware next)
+    {
+      Next = next;
+      return this;
+    }
+
+    IPipelineMiddleware Next { get; set; }
+  }
   class CodeMiddleware : IPipelineMiddleware, IPipelineMiddlewareFactory
   {
     readonly Action _action;
