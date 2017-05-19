22462df8fa6812b523533f084b4e7db67b486749
Sebastien Lambla <seb@serialseb.com>
2017-05-19 17:05:56 +0200

    Move stuff about

--- src/OpenRasta.Testing/SpecExtensions.cs
+++ src/OpenRasta.Testing/SpecExtensions.cs
@@ -15,12 +15,6 @@ namespace OpenRasta.Testing
       values.ShouldAllBe(item => Equals(expected, item));
     }
 
-    public static T LegacyShouldBe<T>(this T valueToAnalyse, T expectedValue)
-    {
-      valueToAnalyse.ShouldBe(expectedValue);
-      return valueToAnalyse;
-    }
-
     public static void LegacyShouldBe<T>(this Type valueToAnalyse)
     {
       valueToAnalyse.ShouldBe(typeof(T));
@@ -36,16 +30,6 @@ namespace OpenRasta.Testing
       obj.ShouldBeAssignableTo<TExpected>();
     }
 
-    public static void LegacyShouldBeEmpty<T>(this IEnumerable<T> values)
-    {
-      values.ShouldBeEmpty();
-    }
-
-    public static bool LegacyShouldBeFalse(this bool value)
-    {
-      value.ShouldBeFalse();
-      return false;
-    }
 
     public static void LegacyShouldBeGreaterThan<T>(this T actual, T expected) where T : IComparable<T>
     {
@@ -55,12 +39,6 @@ namespace OpenRasta.Testing
     public static void LegacyShouldBeNull<T>(this T obj)
     {
       obj.ShouldBeNull();
-    }
-
-    public static TExpected LegacyShouldBeOfType<TExpected>(this object obj)
-    {
-      obj.ShouldBeOfType<TExpected>();
-      return (TExpected) obj;
     }
 
     public static void LegacyShouldBeTheSameInstanceAs(this object actual, object expected)
@@ -78,35 +56,11 @@ namespace OpenRasta.Testing
       codeToExecute();
     }
 
-    public static IDictionary<T, U> LegacyShouldContain<T, U>(this IDictionary<T, U> dic, T key, U value)
-    {
-      dic.ShouldContainKeyAndValue(key, value);
-
-      return dic;
-    }
-
-    public static IEnumerable<T> LegacyShouldContain<T>(this IEnumerable<T> list, T expected)
-    {
-      LegacyShouldContain(list, expected, (t, t2) => t.Equals(t2));
-      return list;
-    }
-
-    public static void LegacyShouldContain<T>(this IEnumerable<T> list, T expected, Func<T, T, bool> match)
+    static void LegacyShouldContain<T>(this IEnumerable<T> list, T expected, Func<T, T, bool> match)
     {
       list.ShouldContain(item => match(item, expected));
     }
 
-    public static string LegacyShouldContain(this string baseString, string textToFind)
-    {
-      baseString.ShouldContain(textToFind,Case.Sensitive);
-      return baseString;
-    }
-
-    public static IEnumerable<T> LegacyShouldHaveCountOf<T>(this IEnumerable<T> values, int count)
-    {
-      values.Count().ShouldBe(count);
-      return values;
-    }
 
     public static void LegacyShouldHaveSameElementsAs(this NameValueCollection actual, NameValueCollection expected)
     {
@@ -121,14 +75,8 @@ namespace OpenRasta.Testing
     public static void LegacyShouldNotBe<T>(this T valueToAnalyse, T expectedValue)
     {
       valueToAnalyse.ShouldNotBe(expectedValue);
-      return;
     }
 
-    public static T legacyShouldNotBeNull<T>(this T obj) where T : class
-    {
-      obj.ShouldNotBeNull();
-      return obj;
-    }
 
     public static void LegacyShouldNotBeTheSameInstanceAs(this object actual, object expected)
     {
@@ -145,9 +93,58 @@ namespace OpenRasta.Testing
       codeToExecute.ShouldThrow<T>();
     }
 
+    public static IDictionary<T, U> LegacyShouldContain<T, U>(this IDictionary<T, U> dic, T key, U value)
+    {
+      dic.ShouldContainKeyAndValue(key, value);
+
+      return dic;
+    }
+
+    public static TExpected LegacyShouldBeOfType<TExpected>(this object obj)
+    {
+      obj.ShouldBeOfType<TExpected>();
+      return (TExpected) obj;
+    }
+
+    public static T LegacyShouldBe<T>(this T valueToAnalyse, T expectedValue)
+    {
+      valueToAnalyse.ShouldBe(expectedValue);
+      return valueToAnalyse;
+    }
+
+    public static bool LegacyShouldBeFalse(this bool value)
+    {
+      value.ShouldBeFalse();
+      return false;
+    }
+
+    public static IEnumerable<T> LegacyShouldContain<T>(this IEnumerable<T> list, T expected)
+    {
+      LegacyShouldContain(list, expected, (t, t2) => t.Equals(t2));
+      return list;
+    }
+
+    public static string LegacyShouldContain(this string baseString, string textToFind)
+    {
+      baseString.ShouldContain(textToFind, Case.Sensitive);
+      return baseString;
+    }
+
+    public static IEnumerable<T> LegacyShouldHaveCountOf<T>(this IEnumerable<T> values, int count)
+    {
+      values.Count().ShouldBe(count);
+      return values;
+    }
+
     public static T LegacyShouldThrow<T>(this Action codeToExecute) where T : Exception
     {
       return codeToExecute.ShouldThrow<T>();
+    }
+
+    public static T legacyShouldNotBeNull<T>(this T obj) where T : class
+    {
+      obj.ShouldNotBeNull();
+      return obj;
     }
   }
 }
