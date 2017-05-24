a038d0aea82653201d5083a693aefcbbfa014d02
Sebastien Lambla <seb@serialseb.com>
2017-05-24 20:36:23 +0200

    Uri matching removes braces in uri params
    see #99

---
+++ src/Tests/Scenarios.Uris/json_encoded_uris.cs
@@ -0,0 +1,31 @@
+using System.Threading.Tasks;
+using OpenRasta.Configuration;
+using OpenRasta.Hosting.InMemory;
+using Shouldly;
+using Tests.Scenarios.HandlerSelection;
+using Xunit;
+
+namespace Tests.Scenarios.Uris
+{
+  public class json_encoded_uris
+  {
+    [Fact(Skip="Issue #99")]
+    public async Task are_processed_correctly()
+    {
+      var response = await (new InMemoryHost(() => ResourceSpace.Has
+          .ResourcesNamed("json")
+          .AtUri("/json?data={data}")
+          .HandledBy<IdentityHandler>())
+        .Get("/json?data=%7B+%22Name%22+%3A+%22pete%22+%7D"));
+      response.ReadString().ShouldBe("{ \"Name\" , \"pete\" }");
+    }
+
+    public class IdentityHandler
+    {
+      public string Get(string data)
+      {
+        return data;
+      }
+    }
+  }
+}
