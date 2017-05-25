f0941085da4d3480ecbabcf73f4874112f9881ed
Sebastien Lambla <seb@serialseb.com>
2017-05-25 02:16:09 +0200

    Repro for extension not overriding content negotiation on error
    see #15

---
+++ src/Tests/Scenarios.Codecs/content_extension_with_throwing_handler.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Threading.Tasks;
+using OpenRasta.Configuration;
+using OpenRasta.Hosting.InMemory;
+using OpenRasta.Web;
+using OpenRasta.Web.UriDecorators;
+using Shouldly;
+using Xunit;
+
+namespace Tests.Scenarios.HandlerSelection.Scenarios.Codecs
+{
+  public class content_extension_with_throwing_handler
+  {
+    [Fact(Skip="Issue #15")]
+    public async Task content_type_in_extension_overrides_conneg()
+    {
+      var server = new InMemoryHost(() =>
+      {
+        ResourceSpace.Uses.UriDecorator<ContentTypeExtensionUriDecorator>();
+        
+        ResourceSpace.Has.ResourcesOfType<object>()
+          .WithoutUri
+          .AsXmlDataContract().ForMediaType(MediaType.Xml.WithQuality(0.9f)).ForExtension("xml")
+          .And
+          .AsJsonDataContract().ForMediaType(MediaType.Json.WithQuality(1.0f)).ForExtension("json");
+
+        ResourceSpace.Has.ResourcesOfType<MyResource>()
+          .AtUri("/stuff")
+          .HandledBy<MyResourceHandler>();
+      });
+      var response = await server.Get("/stuff.xml");
+      response.StatusCode.ShouldBe(500);
+      response.Entity.ContentType.ShouldBe(MediaType.Xml);
+    }
+
+    class MyResourceHandler
+    {
+      public string Get()
+      {
+        throw new InvalidOperationException();
+      }
+    }
+    class MyResource{}
+  }
+}
