ed56ee9800f87180b6e764f556c7b213f28a5491
Sebastien Lambla <seb@serialseb.com>
2017-05-28 18:51:29 +0100

    Di removal Request decoding

--- src/OpenRasta/Pipeline/Contributors/RequestDecoderContributor.cs
+++ src/OpenRasta/Pipeline/Contributors/RequestDecoderContributor.cs
@@ -1,22 +1,19 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.Remoting.Channels;
 using System.Threading.Tasks;
-using OpenRasta.DI;
 using OpenRasta.OperationModel;
 using OpenRasta.OperationModel.Hydrators;
 using OpenRasta.Web;
-using OpenRasta.Pipeline;
 
 namespace OpenRasta.Pipeline.Contributors
 {
   public class RequestDecoderContributor : KnownStages.IRequestDecoding
   {
-    readonly Func<IEnumerable<IOperationAsync>, Task<Tuple<RequestReadResult,IOperationAsync>>> DecodeRequest;
-    public RequestDecoderContributor(IDependencyResolver resolver)
+    readonly Func<IRequestEntityReader> _reader;
+    
+    public RequestDecoderContributor(Func<IRequestEntityReader> reader)
     {
-      DecodeRequest = ops => resolver.Resolve<IRequestEntityReader>().Read(ops);
+      _reader = reader;
     }
 
     public void Initialize(IPipeline pipelineRunner)
@@ -26,7 +23,7 @@ namespace OpenRasta.Pipeline.Contributors
 
     async Task<PipelineContinuation> ReadRequestEntityBody(ICommunicationContext ctx)
     {
-      var operation = await DecodeRequest(ctx.PipelineData.OperationsAsync);
+      var operation = await _reader().Read(ctx.PipelineData.OperationsAsync);
 
       ctx.PipelineData.OperationsAsync = operation.Item1 != RequestReadResult.Success
         ? Enumerable.Empty<IOperationAsync>()
