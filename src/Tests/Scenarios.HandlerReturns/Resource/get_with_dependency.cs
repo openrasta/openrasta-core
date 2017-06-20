e22acb6ba6482ecb2eed37c171b5c0386acee8c8
neil <neil.mosafi@mavensecurities.com>
2017-06-20 08:36:48 +0100

    Add scenarios to test GETting from handlers
    One particular scenario which fails is when an Task based method is handling a request and the handler takes a dependency. This would need to be resolved before merging to the master branch

---
+++ src/Tests/Scenarios.HandlerReturns/Resource/get_with_dependency.cs
@@ -0,0 +1,59 @@
+﻿using System.Threading.Tasks;
+using OpenRasta.Configuration;
+using OpenRasta.Hosting.InMemory;
+using OpenRasta.Web;
+using Shouldly;
+using Tests.Scenarios.HandlerSelection;
+using Xunit;
+
+namespace Tests.Scenarios.HandlerReturns.Resource
+{
+  public class get_with_dependency
+  {
+    InMemoryHost server;
+
+    public get_with_dependency()
+    {
+      server = new InMemoryHost(() =>
+        ResourceSpace.Has
+          .ResourcesOfType<string>()
+          .AtUri("/string-sync").Named("sync")
+          .And.AtUri("/string-async").Named("async")
+          .HandledBy<Handler>());
+    }
+
+    [Fact]
+    public async Task get_with_sync_method()
+    {
+      var response = await server.Get("/string-sync");
+      response.ReadString().ShouldBe("helloworld");
+    }
+
+    [Fact]
+    public async Task get_with_async_method()
+    {
+      var response = await server.Get("/string-async");
+      response.ReadString().ShouldBe("helloworld");
+    }
+
+    class Handler
+    {
+      public Handler(IRequest request)
+      {
+        
+      }
+
+      [HttpOperation(ForUriName = "sync")]
+      public OperationResult GetSync()
+      {
+        return new OperationResult.OK("helloworld");
+      }
+
+      [HttpOperation(ForUriName = "async")]
+      public Task<OperationResult> GetAsync()
+      {
+        return Task.FromResult<OperationResult>(new OperationResult.OK("helloworld"));
+      }
+    }
+  }
+}
---
+++ src/Tests/Scenarios.HandlerReturns/Resource/get_without_dependency.cs
@@ -0,0 +1,54 @@
+﻿using System.Threading.Tasks;
+using OpenRasta.Configuration;
+using OpenRasta.Hosting.InMemory;
+using OpenRasta.Web;
+using Shouldly;
+using Tests.Scenarios.HandlerSelection;
+using Xunit;
+
+namespace Tests.Scenarios.HandlerReturns.Resource
+{
+  public class get_without_dependency
+  {
+    InMemoryHost server;
+
+    public get_without_dependency()
+    {
+      server = new InMemoryHost(() =>
+        ResourceSpace.Has
+          .ResourcesOfType<string>()
+          .AtUri("/string-sync").Named("sync")
+          .And.AtUri("/string-async").Named("async")
+          .HandledBy<Handler>());
+    }
+
+    [Fact]
+    public async Task get_with_sync_method()
+    {
+      var response = await server.Get("/string-sync");
+      response.ReadString().ShouldBe("helloworld");
+    }
+
+    [Fact]
+    public async Task get_with_async_method()
+    {
+      var response = await server.Get("/string-async");
+      response.ReadString().ShouldBe("helloworld");
+    }
+
+    class Handler
+    {
+      [HttpOperation(ForUriName = "sync")]
+      public OperationResult GetSync()
+      {
+        return new OperationResult.OK("helloworld");
+      }
+
+      [HttpOperation(ForUriName = "async")]
+      public Task<OperationResult> GetAsync()
+      {
+        return Task.FromResult<OperationResult>(new OperationResult.OK("helloworld"));
+      }
+    }
+  }
+}
