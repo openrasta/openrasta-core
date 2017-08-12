da3ab2cb592aedfbc84f0e5177a93e6d9d3600bd
Sebastien Lambla <seb@serialseb.com>
2017-08-12 18:30:26 +0100

    Ensure slow-creating singletons don't break things

--- src/OpenRasta/DI/Internal/SingletonLifetimeManager.cs
+++ src/OpenRasta/DI/Internal/SingletonLifetimeManager.cs
@@ -1,13 +1,14 @@
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Threading;
 using OpenRasta.Collections;
 
 namespace OpenRasta.DI.Internal
 {
   public class SingletonLifetimeManager : DependencyLifetimeManager
   {
-    readonly ConcurrentDictionary<string, object> _instances = new ConcurrentDictionary<string, object>();
+    readonly ConcurrentDictionary<string, Lazy<object>> _instances = new ConcurrentDictionary<string, Lazy<object>>();
 
     public SingletonLifetimeManager(InternalDependencyResolver builder)
       : base(builder)
@@ -16,14 +17,22 @@ namespace OpenRasta.DI.Internal
 
     public override object Resolve(ResolveContext context, DependencyRegistration registration)
     {
-      return _instances.GetOrAdd(registration.Key, key => CreateObject(context, registration));
+      var lazy = _instances
+        .GetOrAdd(registration.Key, key => ThreadSafeLazyFactory(context, registration));
+      return lazy.Value;
+    }
+
+    private static Lazy<object> ThreadSafeLazyFactory(ResolveContext context, DependencyRegistration registration)
+    {
+      return new Lazy<object>(() => CreateObject(context, registration), LazyThreadSafetyMode.ExecutionAndPublication);
     }
 
     public override void VerifyRegistration(DependencyRegistration registration)
     {
       if (!registration.IsInstanceRegistration) return;
-      
-      if (!_instances.TryAdd(registration.Key, registration.Instance))
+
+      var instance = registration.Instance;
+      if (!_instances.TryAdd(registration.Key, new Lazy<object>(()=>instance)))
         throw new InvalidOperationException(
           "Trying to register an instance for a registration that already has one.");
       registration.Instance = null;
