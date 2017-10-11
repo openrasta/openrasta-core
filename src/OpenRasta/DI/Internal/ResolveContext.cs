fca46a4996f4d6f8df60ae4fc6bb66d84a95c4e9
Sebastien Lambla <seb@serialseb.com>
2017-10-11 16:51:47 +0100

    Lower knowledge of registrations

--- src/OpenRasta/DI/Internal/ResolveContext.cs
+++ src/OpenRasta/DI/Internal/ResolveContext.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace OpenRasta.DI.Internal
 {
@@ -12,12 +13,12 @@ namespace OpenRasta.DI.Internal
       Registrations = registrations;
     }
 
-    public IDependencyRegistrationCollection Registrations { get; }
+    private IDependencyRegistrationCollection Registrations { get; }
 
     public bool TryResolve(Type serviceType, out object instance)
     {
       instance = null;
-      var profile = ResolveProfile.FindProfile(serviceType, this);
+      var profile = TryGetProfile(serviceType, this);
 
       return profile != null && profile.TryResolve(out instance);
     }
@@ -37,6 +38,52 @@ namespace OpenRasta.DI.Internal
       {
         _recursionDefender.Pop();
       }
+    }
+
+    private static ResolveProfile TryGetSimpleProfile(ResolveContext ctx, Type serviceType)
+    {
+      var registration = ctx.Registrations.DefaultRegistrationFor(serviceType);
+      return registration != null ? new SimpleProfile(registration, ctx) : null;
+    }
+
+    private static ResolveProfile TryGetEnumProfile(ResolveContext ctx, Type serviceType)
+    {
+      
+      if (!serviceType.IsGenericType ||
+          serviceType.IsGenericTypeDefinition ||
+          serviceType.GetGenericTypeDefinition() != typeof(IEnumerable<>))
+        return null;
+
+      var innerServiceType = serviceType.GetGenericArguments()[0];
+
+      var registrations = ctx.Registrations[serviceType];
+      if (registrations.Any() == false) return null;
+      
+      return (ResolveProfile) Activator
+        .CreateInstance(
+          typeof(EnumerableProfile<>)
+            .MakeGenericType(innerServiceType),
+          registrations, ctx);
+    }
+
+    private static ResolveProfile TryGetFuncProfile(ResolveContext ctx, Type serviceType)
+    {
+      if (serviceType.IsGenericType == false
+          || serviceType.GetGenericTypeDefinition() != typeof(Func<>))
+        return null;
+      
+      var innerType = serviceType.GetGenericArguments()[0];
+      var innerProfile = TryGetProfile(innerType, ctx);
+      return innerProfile == null
+        ? null
+        : (ResolveProfile)Activator.CreateInstance(typeof(FuncProfile<>).MakeGenericType(innerType), innerProfile);
+    }
+
+    private static ResolveProfile TryGetProfile(Type serviceType, ResolveContext resolveContext)
+    {
+      return TryGetSimpleProfile(resolveContext, serviceType)
+             ?? TryGetEnumProfile(resolveContext, serviceType)
+             ?? TryGetFuncProfile(resolveContext, serviceType);
     }
   }
 }
--- src/OpenRasta/DI/Internal/ResolveProfile.cs
+++ src/OpenRasta/DI/Internal/ResolveProfile.cs
@@ -6,52 +6,6 @@ namespace OpenRasta.DI.Internal
 {
   abstract class ResolveProfile
   {
-    static ResolveProfile Simple(ResolveContext ctx, Type serviceType)
-    {
-      var registration = ctx.Registrations.DefaultRegistrationFor(serviceType);
-      return registration != null ? new SimpleProfile(registration, ctx) : null;
-    }
-
-    static ResolveProfile Enumerable(ResolveContext ctx, Type serviceType)
-    {
-      
-      if (!serviceType.IsGenericType ||
-          serviceType.IsGenericTypeDefinition ||
-          serviceType.GetGenericTypeDefinition() != typeof(IEnumerable<>))
-        return null;
-
-      var innerServiceType = serviceType.GetGenericArguments()[0];
-
-      var registrations = ctx.Registrations[serviceType];
-      if (registrations.Any() == false) return null;
-      
-      return (ResolveProfile) Activator
-        .CreateInstance(
-          typeof(EnumerableProfile<>)
-          .MakeGenericType(innerServiceType),
-          registrations, ctx);
-    }
-
-    static ResolveProfile Func(ResolveContext ctx, Type serviceType)
-    {
-      if (serviceType.IsGenericType == false
-          || serviceType.GetGenericTypeDefinition() != typeof(Func<>))
-        return null;
-      
-      var innerType = serviceType.GetGenericArguments()[0];
-      var innerProfile = FindProfile(innerType, ctx);
-      return innerProfile == null
-      ? null
-      : (ResolveProfile)Activator.CreateInstance(typeof(FuncProfile<>).MakeGenericType(innerType), innerProfile);
-    }
-
     public abstract bool TryResolve(out object instance);
-
-    public static ResolveProfile FindProfile(Type serviceType, ResolveContext resolveContext)
-    {
-      return Simple(resolveContext, serviceType)
-             ?? Enumerable(resolveContext, serviceType)
-             ?? Func(resolveContext, serviceType);
-    }
   }
 }
