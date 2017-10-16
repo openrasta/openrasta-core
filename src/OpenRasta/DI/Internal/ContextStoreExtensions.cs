5569a699e72bfca29a90818512ebd6c6dbf21e45
Sebastien Lambla <seb@serialseb.com>
2017-10-16 15:07:19 +0100

    Bringing back some old classes and interfaces

--- src/OpenRasta/DI/Internal/ContextStoreExtensions.cs
+++ src/OpenRasta/DI/Internal/ContextStoreExtensions.cs
@@ -1,13 +1,47 @@
+using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using OpenRasta.Pipeline;
 
 namespace OpenRasta.DI.Internal
 {
+  [Obsolete("Internal class no longer in use.")]
+  public class ContextStoreDependency
+  {
+    public ContextStoreDependency(string key, object instance, IContextStoreDependencyCleaner cleaner)
+    {
+      if (key == null) throw new ArgumentNullException("key");
+      if (instance == null) throw new ArgumentNullException("instance");
+      Key = key;
+      Instance = instance;
+      Cleaner = cleaner;
+    }
+
+    public IContextStoreDependencyCleaner Cleaner { get; set; }
+    public object Instance { get; set; }
+    public string Key { get; set; }
+  }
+  
   public static class ContextStoreExtensions
   {
     private const string CTX_INSTANCES_KEY_CONCURRENT = nameof(CTX_INSTANCES_KEY_CONCURRENT);
 
+    
+    [Obsolete("Internal no longer in use.")]
+    public static void Destruct(this IContextStore store)
+    {
+      foreach (var dep in store.GetContextInstances())
+          dep.Cleaner?.Destruct(dep.Key, dep.Instance);
+      store.GetContextInstances().Clear();
+    }
+    
+    [Obsolete("Internal no longer in use.")]
+    public static IList<ContextStoreDependency> GetContextInstances(this IContextStore store)
+    {
+      return (IList<ContextStoreDependency>)
+        (store["__OR_CTX_INSTANCES_KEY"] ?? (store["__OR_CTX_INSTANCES_KEY"] = new List<ContextStoreDependency>()));
+    }
+    
     public static ConcurrentDictionary<DependencyRegistration, object> GetConcurrentContextInstances(
       this IContextStore store)
     {
