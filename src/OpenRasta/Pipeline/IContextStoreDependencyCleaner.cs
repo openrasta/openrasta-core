e9b763737264521f9d63a5c48402e9fd436c7bf1
Sebastien Lambla <seb@serialseb.com>
2017-10-16 14:26:40 +0100

    Bring back the interface to its previous version

--- src/OpenRasta/Pipeline/IContextStoreDependencyCleaner.cs
+++ src/OpenRasta/Pipeline/IContextStoreDependencyCleaner.cs
@@ -2,8 +2,8 @@ using OpenRasta.DI.Internal;
 
 namespace OpenRasta.Pipeline
 {
-    public interface IContextStoreDependencyCleaner
-    {
-        void UnregisterTemporaryRegistration(DependencyRegistration registration, object instance);
-    }
+  public interface IContextStoreDependencyCleaner
+  {
+    void Destruct(string key, object instance);
+  }
 }
