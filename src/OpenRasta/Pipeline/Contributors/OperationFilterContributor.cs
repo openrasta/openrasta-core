ad5dd161a5276cf65e9966dbac23d9fc632d80d2
Greg Shackles <gshackles@gmail.com>
2017-12-20 14:38:29 -0500

    OperationFilterContributor: restore old filter ordering

--- src/OpenRasta/Pipeline/Contributors/OperationFilterContributor.cs
+++ src/OpenRasta/Pipeline/Contributors/OperationFilterContributor.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using OpenRasta.DI;
 using OpenRasta.OperationModel;
 using OpenRasta.OperationModel.Filters;
@@ -17,5 +19,17 @@ namespace OpenRasta.Pipeline.Contributors
         {
             pipeline.After<KnownStages.IOperationCreation>();
         }
+
+        protected override IEnumerable<IOperationFilter> OrderProcessors(IEnumerable<IOperationFilter> operations) =>
+            operations.OrderBy(operation =>
+            {
+                switch (operation)
+                {
+                    case HttpMethodOperationFilter _: return 0;
+                    case UriNameOperationFilter _: return 1;
+                    case UriParametersFilter _: return 2;
+                    default: return 3;
+                }
+            });
     }
 }
