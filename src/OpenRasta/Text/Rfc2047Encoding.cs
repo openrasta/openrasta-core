7fa34edde30295fc067f45ee5962d9ecee270ed1
Sebastien Lambla <seb@serialseb.com>
2017-12-29 22:29:51 +0100

    Rewrite rfc 2047 encoding

--- src/OpenRasta/Text/Rfc2047Encoding.cs
+++ src/OpenRasta/Text/Rfc2047Encoding.cs
@@ -1,138 +1,221 @@
-#region License
-/* Authors:
- *      Sebastien Lambla (seb@serialseb.com)
- * Copyright:
- *      (C) 2007-2009 Caffeine IT & naughtyProd Ltd (http://www.caffeine-it.com)
- * License:
- *      This file is distributed under the terms of the MIT License found at the end of this file.
- */
-#endregion
 using System;
-using System.Collections.Generic;
+using System.Data.SqlTypes;
 using System.Text;
 using System.Globalization;
 using System.IO;
+using OpenRasta.Pipeline.Contributors;
 
 namespace OpenRasta.Text
 {
-    /// <summary>
-    /// Provides partial implementation for decoding strings according to RFC2047.
-    /// </summary>
-    /// <remarks>
-    /// This implementation is not yet conformant to rfc2047.
-    /// </remarks>
-    public static class Rfc2047Encoding
+  /// <summary>
+  /// Provides partial implementation for decoding strings according to RFC2047.
+  /// </summary>
+  /// <remarks>
+  /// This implementation is not yet conformant to rfc2047.
+  /// </remarks>
+  public static class Rfc2047Encoding
+  {
+    class Rfc2047Decoder
     {
-        public static string DecodeTextToken(string textToDecode)
+      int _currentPosition;
+      readonly string _text;
+
+      public Rfc2047Decoder(string text)
+      {
+        _text = text;
+        _currentPosition = 0;
+      }
+
+      bool TryReadToken(char first, char second)
+      {
+        if (!IsNext(first, second)) return false;
+
+        _currentPosition += 2;
+        return true;
+      }
+
+      delegate bool TryDecode(Encoding encoder, out string output);
+
+      bool TryGetDecoder(out TryDecode decoder)
+      {
+        decoder = null;
+        if (TryReadToken('Q', '?') || TryReadToken('q', '?'))
         {
-            StringBuilder decoded = new StringBuilder();
-            StringBuilder charsetBuilder = null;
-            StringBuilder encodedText = null;
-
-            for (int i = 0; i < textToDecode.Length; i++)
-            {
-                char ch = textToDecode[i];
-
-                if (ch == '=' && i < textToDecode.Length - 1 && textToDecode[i + 1] == '?')
-                {
-                    i += 2;
-                    charsetBuilder = new StringBuilder();
-                    while (i < textToDecode.Length && textToDecode[i] != '?')
-                    {
-                        charsetBuilder.Append(textToDecode[i]);
-                        i++;
-                    }
-                    i++;
-                    string charset = charsetBuilder.ToString();
-                    Encoding textEncoder = null;
-                    try
-                    {
-                        textEncoder = Encoding.GetEncoding(charset);
-                    }
-                    catch { }
-                    char encoding = textToDecode[i];
-                    Func<string, Encoding, string> decoder = null;
-                    if ((encoding == 'Q' || encoding == 'q') && i + 1 < textToDecode.Length && textToDecode[i + 1] == '?')
-                    { decoder = DecodeQuotedPrintable; }
-                    else if ((encoding == 'B' || encoding == 'b') && i + 1 < textToDecode.Length && textToDecode[i + 1] == '?')
-                    { decoder = DecodeBase64; }
-
-                    if (textEncoder != null && decoder != null)
-                    {
-                        i += 2;
-                        encodedText = new StringBuilder();
-                        byte[] encodedBuffer = new byte[4];
-                        for (; i + 1 < textToDecode.Length && !(textToDecode[i] == '?' && textToDecode[i + 1] == '='); i++)
-                        {
-                            encodedText.Append(textToDecode[i]);
-                        }
-                        decoded.Append(decoder(encodedText.ToString(),textEncoder));
-                        i += 1;
-                    }
-                    else
-                    {
-                        decoded.Append("=?").Append(charset).Append("?").Append(encoding);
-                        continue;
-                    }
-                }
-                else
-                    decoded.Append(ch);
-
-            }
-            return decoded.ToString();
+          decoder = TryDecodeQuoted;
+          return true;
         }
-        private static string DecodeQuotedPrintable(string textToDecode, Encoding textEncoder)
+
+        if (TryReadToken('B', '?') || TryReadToken('b', '?'))
         {
-            MemoryStream toDecode = new MemoryStream(textToDecode.Length);
-
-            for (int i = 0; i < textToDecode.Length; i++)
-            {
-                byte byteToAdd = 0;
-                if (textToDecode[i] == '_')
-                {
-                    byteToAdd = (byte)' ';
-                }
-                else if (textToDecode[i] == '=' && i + 2 < textToDecode.Length)
-                {
-                    byteToAdd = byte.Parse(textToDecode[i+1] + "" + textToDecode[i + 2], NumberStyles.HexNumber,CultureInfo.InvariantCulture);
-                    i += 2;
-                }
-                else
-                {
-                    byteToAdd = (byte)textToDecode[i];
-                }
-                toDecode.WriteByte(byteToAdd);
-            }
-
-            return textEncoder.GetString(toDecode.GetBuffer(), 0, (int)toDecode.Length);
+          decoder = TryDecodeBase64;
+          return true;
         }
-        private static string DecodeBase64(string text, Encoding textEncoder)
+
+        return false;
+      }
+
+      int LengthToEndOfToken()
+      {
+        for (var i = _currentPosition; i < _text.Length - 1; i++)
         {
-            var bytes = Convert.FromBase64String(text);
-            return textEncoder.GetString(bytes);
+          if ((_text[i] == '?' && _text[i + 1] == '='))
+            return i - _currentPosition;
         }
+
+        return -1;
+      }
+
+      bool TryDecodeBase64(Encoding encoder, out string output)
+      {
+        output = null;
+
+        var length = LengthToEndOfToken();
+        if (length == -1) return false;
+
+
+        var restore = _currentPosition;
+
+        try
+        {
+          var bytes = Convert.FromBase64String(_text.Substring(_currentPosition, length));
+          output = encoder.GetString(bytes);
+          _currentPosition += length+2;
+          return true;
+        }
+        catch
+        {
+          _currentPosition = restore;
+          return false;
+        }
+      }
+
+      int CharsLeft => _text.Length - _currentPosition;
+
+      bool TryDecodeQuoted(Encoding encoder, out string output)
+      {
+        output = null;
+
+        var originalCharacters = LengthToEndOfToken();
+        
+        if (originalCharacters == -1) return false;
+
+        var binary = new byte[originalCharacters];
+        var restore = _currentPosition;
+        
+        var parsedCharacters = originalCharacters;
+        for (var i = 0; i < parsedCharacters; i++)
+        {
+          switch (Read())
+          {
+            case '_':
+              binary[i] = (byte) ' ';
+              break;
+            case '=' when CharsLeft >= 2:
+              binary[i] = byte.Parse(Read() + "" + Read(), NumberStyles.HexNumber,
+                CultureInfo.InvariantCulture);
+              parsedCharacters -= 2;
+              break;
+            case var any:
+              binary[i] = (byte) any;
+              break;
+          }
+        }
+
+        try
+        {
+          output = encoder.GetString(binary, 0, parsedCharacters);
+          _currentPosition += 2;
+          return true;
+        }
+        catch
+        {
+          _currentPosition = restore;
+          return false;
+        }
+      }
+
+      public string Decode()
+      {
+        var sb = new StringBuilder();
+        while (HasCharacters)
+        {
+          var pos = _currentPosition;
+          if (TryReadToken('=', '?') &&
+              TryParseEncoding(out var encoder) &&
+              TryGetDecoder(out var tryDecode) &&
+              tryDecode(encoder, out var decodedToken))
+          {
+            sb.Append(decodedToken);
+          }
+          else
+          {
+            _currentPosition = pos;
+            sb.Append(Read());
+          }
+        }
+
+        return sb.ToString();
+      }
+
+      char Current => _text[_currentPosition];
+
+      char Read()
+      {
+        if (!HasCharacters) throw new InvalidOperationException("Reached the end of the string");
+        _currentPosition++;
+        return _text[_currentPosition - 1];
+      }
+
+      bool TryParseEncoding(out Encoding encoder)
+      {
+        encoder = null;
+
+        var previousPosition = _currentPosition;
+
+        var charsetBuilder = new StringBuilder();
+        while (HasCharacters)
+        {
+          var ch = Read();
+
+          if (ch == '?')
+          {
+            if (TryGetEncoding(charsetBuilder.ToString(), out encoder))
+              return true;
+            break;
+          }
+
+          charsetBuilder.Append(ch);
+        }
+
+        _currentPosition = previousPosition;
+        return false;
+      }
+
+      bool TryGetEncoding(string text, out Encoding encoder)
+      {
+        encoder = null;
+        try
+        {
+          encoder = Encoding.GetEncoding(text);
+          return true;
+        }
+        catch
+        {
+          return false;
+        }
+      }
+
+      bool IsNext(char first, char second)
+        => CharsLeft >= 2 &&
+           _text[_currentPosition] == first &&
+           _text[_currentPosition + 1] == second;
+
+      bool HasCharacters => _currentPosition < _text.Length;
     }
-}
 
-#region Full license
-//
-// Permission is hereby granted, free of charge, to any person obtaining
-// a copy of this software and associated documentation files (the
-// "Software"), to deal in the Software without restriction, including
-// without limitation the rights to use, copy, modify, merge, publish,
-// distribute, sublicense, and/or sell copies of the Software, and to
-// permit persons to whom the Software is furnished to do so, subject to
-// the following conditions:
-// 
-// The above copyright notice and this permission notice shall be
-// included in all copies or substantial portions of the Software.
-// 
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-//
-#endregion
+    public static string DecodeTextToken(string textToDecode)
+    {
+      return new Rfc2047Decoder(textToDecode).Decode();
+    }
+  }
+}
