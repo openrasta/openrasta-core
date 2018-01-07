4b60d63df66da7e917c439f1ef688494aace5507
Sebastien Lambla <seb@serialseb.com>
2018-01-07 13:41:38 +0000

    Cleanup code

--- src/OpenRasta/Pipeline/PipelineData.cs
+++ src/OpenRasta/Pipeline/PipelineData.cs
@@ -1,20 +1,6 @@
-#region License
-
-/* Authors:
- *      Sebastien Lambla (seb@serialseb.com)
- * Copyright:
- *      (C) 2007-2009 Caffeine IT & naughtyProd Ltd (http://www.caffeine-it.com)
- * License:
- *      This file is distributed under the terms of the MIT License found at the end of this file.
- */
-
-#endregion
-
 using System;
 using System.Collections.Generic;
-using System.Collections.Specialized;
 using System.Linq;
-using System.Reflection;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using OpenRasta.Codecs;
@@ -52,8 +38,8 @@ namespace OpenRasta.Pipeline
     /// </summary>
     public Type HandlerType
     {
-      get { return SafeGet<Type>(HANDLER_TYPE); }
-      set { base[HANDLER_TYPE] = value; }
+      get => SafeGet<Type>(HANDLER_TYPE);
+      set => base[HANDLER_TYPE] = value;
     }
 
     public X509Certificate ClientCertificate
@@ -70,14 +56,14 @@ namespace OpenRasta.Pipeline
 
     public TaskCompletionSource<object> Suspend
     {
-      get { return SafeGet<TaskCompletionSource<object>>(SUSPEND); }
-      set { base[SUSPEND] = value; }
+      get => SafeGet<TaskCompletionSource<object>>(SUSPEND);
+      set => base[SUSPEND] = value;
     }
 
     public TaskCompletionSource<object> Resume
     {
-      get { return SafeGet<TaskCompletionSource<object>>(RESUME);}
-      set { base[RESUME] = value; }
+      get => SafeGet<TaskCompletionSource<object>>(RESUME);
+      set => base[RESUME] = value;
     }
     public IEnumerable<IOperationAsync> OperationsAsync
     {
@@ -101,8 +87,8 @@ namespace OpenRasta.Pipeline
     /// </summary>
     public object ResourceKey
     {
-      get { return SafeGet<object>(RESOURCE_KEY); }
-      set { base[RESOURCE_KEY] = value; }
+      get => SafeGet<object>(RESOURCE_KEY);
+      set => base[RESOURCE_KEY] = value;
     }
 
     /// <summary>
@@ -110,14 +96,14 @@ namespace OpenRasta.Pipeline
     /// </summary>
     public CodecRegistration ResponseCodec
     {
-      get { return SafeGet<CodecRegistration>(RESPONSE_CODEC); }
-      set { base[RESPONSE_CODEC] = value; }
+      get => SafeGet<CodecRegistration>(RESPONSE_CODEC);
+      set => base[RESPONSE_CODEC] = value;
     }
 
     public ICollection<IType> SelectedHandlers
     {
-      get { return SafeGet<ICollection<IType>>(SELECTED_HANDLERS); }
-      set { base[SELECTED_HANDLERS] = value; }
+      get => SafeGet<ICollection<IType>>(SELECTED_HANDLERS);
+      set => base[SELECTED_HANDLERS] = value;
     }
 
     /// <summary>
@@ -125,47 +111,25 @@ namespace OpenRasta.Pipeline
     /// </summary>
     public UriRegistration SelectedResource
     {
-      get { return SafeGet<UriRegistration>(SELECTED_RESOURCE); }
-      set { base[SELECTED_RESOURCE] = value; }
+      get => SafeGet<UriRegistration>(SELECTED_RESOURCE);
+      set => base[SELECTED_RESOURCE] = value;
     }
 
     public PipelineStage PipelineStage
     {
-      get { return SafeGet<PipelineStage>(PIPELINE_STATE); }
-      set { base[PIPELINE_STATE] = value; }
+      get => SafeGet<PipelineStage>(PIPELINE_STATE);
+      set => base[PIPELINE_STATE] = value;
     }
 
     public new object this[object key]
     {
-      get { return ContainsKey(key) ? base[key] : null; }
-      set { base[key] = value; }
+      get => ContainsKey(key) ? base[key] : null;
+      set => base[key] = value;
     }
 
     T SafeGet<T>(string key) where T : class
     {
-      object o;
-      return TryGetValue(key, out o) ? o as T : null;
+      return TryGetValue(key, out var o) ? o as T : null;
     }
   }
 }
-
-#region Full license
-
-// Permission is hereby granted, free of charge, to any person obtaining
-// a copy of this software and associated documentation files (the
-// "Software"), to deal in the Software without restriction, including
-// without limitation the rights to use, copy, modify, merge, publish,
-// distribute, sublicense, and/or sell copies of the Software, and to
-// permit persons to whom the Software is furnished to do so, subject to
-// the following conditions:
-// The above copyright notice and this permission notice shall be
-// included in all copies or substantial portions of the Software.
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
-#endregion
