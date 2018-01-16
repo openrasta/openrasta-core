76d6f0fa263aa55e7141668baa4c50511bb87bfd
Sebastien Lambla <seb@serialseb.com>
2018-01-16 21:42:26 +0100

    Fix tests for the swap to ambientcontext for inmem

--- src/OpenRasta/Codecs/multipart/form-data/MultipartFormDataObjectCodec.cs
+++ src/OpenRasta/Codecs/multipart/form-data/MultipartFormDataObjectCodec.cs
@@ -1,4 +1,5 @@
 #region License
+
 /* Authors:
  *      Sebastien Lambla (seb@serialseb.com)
  * Copyright:
@@ -6,6 +7,7 @@
  * License:
  *      This file is distributed under the terms of the MIT License found at the end of this file.
  */
+
 #endregion
 
 using System;
@@ -19,40 +21,48 @@ using OpenRasta.Web;
 
 namespace OpenRasta.Codecs
 {
-    [MediaType("multipart/form-data;q=0.5")]
-    [SupportedType(typeof(IEnumerable<IMultipartHttpEntity>))]
-    [SupportedType(typeof(IDictionary<string, IList<IMultipartHttpEntity>>))]
-    public class MultipartFormDataObjectCodec : AbstractMultipartFormDataCodec, IMediaTypeReader
+  [MediaType("multipart/form-data;q=0.5")]
+  [SupportedType(typeof(IEnumerable<IMultipartHttpEntity>))]
+  [SupportedType(typeof(IDictionary<string, IList<IMultipartHttpEntity>>))]
+  public class MultipartFormDataObjectCodec : AbstractMultipartFormDataCodec, IMediaTypeReader
+  {
+    public MultipartFormDataObjectCodec(
+      ICommunicationContext context,
+      ICodecRepository codecs,
+      IDependencyResolver container,
+      ITypeSystem typeSystem,
+      IObjectBinderLocator binderLocator)
+      : base(context, codecs, container, typeSystem, binderLocator)
     {
-        public MultipartFormDataObjectCodec(ICommunicationContext context, ICodecRepository codecs, IDependencyResolver container, ITypeSystem typeSystem, IObjectBinderLocator binderLocator)
-            : base(context, codecs, container, typeSystem, binderLocator)
-        {
-        }
-
-        public object ReadFrom(IHttpEntity request, IType destinationType, string parameterName)
-        {
-            if (destinationType.IsAssignableFrom<IEnumerable<IMultipartHttpEntity>>())
-            {
-                var multipartReader = new MultipartReader(request.ContentType.Boundary, request.Stream);
-                return multipartReader.GetParts();
-            }
-            if (destinationType.IsAssignableFrom<IDictionary<string, IList<IMultipartHttpEntity>>>())
-            {
-                return FormData(request);
-            }
-            var binder = BinderLocator.GetBinder(destinationType);
-            if (binder == null)
-                throw new InvalidOperationException("Cannot find a binder to create the object");
-            binder.Prefixes.Add(parameterName);
-            bool wasAnyKeyUsed = ReadKeyValues(request).Aggregate(false, (wasUsed, kv) => kv.SetProperty(binder) || wasUsed);
-            var result = binder.BuildObject();
-
-            return wasAnyKeyUsed && result.Successful ? result.Instance : Missing.Value;
-        }
     }
+
+    public object ReadFrom(IHttpEntity request, IType destinationType, string parameterName)
+    {
+      if (destinationType.IsAssignableFrom<IEnumerable<IMultipartHttpEntity>>())
+      {
+        var multipartReader = new MultipartReader(request.ContentType.Boundary, request.Stream);
+        return multipartReader.GetParts();
+      }
+
+      if (destinationType.IsAssignableFrom<IDictionary<string, IList<IMultipartHttpEntity>>>())
+      {
+        return FormData(request);
+      }
+
+      var binder = BinderLocator.GetBinder(destinationType);
+      if (binder == null)
+        throw new InvalidOperationException("Cannot find a binder to create the object");
+      binder.Prefixes.Add(parameterName);
+      bool wasAnyKeyUsed = ReadKeyValues(request).Aggregate(false, (wasUsed, kv) => kv.SetProperty(binder) || wasUsed);
+      var result = binder.BuildObject();
+
+      return wasAnyKeyUsed && result.Successful ? result.Instance : Missing.Value;
+    }
+  }
 }
 
 #region Full license
+
 // Permission is hereby granted, free of charge, to any person obtaining
 // a copy of this software and associated documentation files (the
 // "Software"), to deal in the Software without restriction, including
@@ -69,4 +79,5 @@ namespace OpenRasta.Codecs
 // LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
 // OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 #endregion
