fc5f9d0355356e9bd376b8a0813ff3e37bd2bc7e
Sebastien Lambla <seb@serialseb.com>
2018-01-20 00:03:01 +0100

    Remove more DR

--- src/OpenRasta/Pipeline/Contributors/HandlerResolverContributor.cs
+++ src/OpenRasta/Pipeline/Contributors/HandlerResolverContributor.cs
@@ -1,11 +1,7 @@
-
-using System;
 using System.Linq;
-using OpenRasta.DI;
 using OpenRasta.Handlers;
 using OpenRasta.TypeSystem;
 using OpenRasta.Web;
-using OpenRasta.Pipeline;
 
 namespace OpenRasta.Pipeline.Contributors
 {
@@ -14,12 +10,10 @@ namespace OpenRasta.Pipeline.Contributors
   /// </summary>
   public class HandlerResolverContributor : KnownStages.IHandlerSelection
   {
-    readonly IDependencyResolver _resolver;
     readonly IHandlerRepository _handlers;
 
-    public HandlerResolverContributor(IDependencyResolver resolver, IHandlerRepository repository)
+    public HandlerResolverContributor(IHandlerRepository repository)
     {
-      _resolver = resolver;
       _handlers = repository;
     }
 
@@ -39,28 +33,3 @@ namespace OpenRasta.Pipeline.Contributors
     }
   }
 }
-
-#region Full license
-
-//
-// Permission is hereby granted, free of charge, to any person obtaining
-// a copy of this software and associated documentation files (the
-// "Software"), to deal in the Software without restriction, including
-// without limitation the rights to use, copy, modify, merge, publish,
-// distribute, sublicense, and/or sell copies of the Software, and to
-// permit persons to whom the Software is furnished to do so, subject to
-// the following conditions:
-// 
-// The above copyright notice and this permission notice shall be
-// included in all copies or substantial portions of the Software.
-// 
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-//
-
-#endregion
