7badfdcc0fde7f705350057822fb9e4812957f33
Sebastien Lambla <seb@serialseb.com>
2018-01-20 00:00:58 +0100

    Remove more DR

--- src/OpenRasta/Pipeline/Contributors/OperationFilterContributor.cs
+++ src/OpenRasta/Pipeline/Contributors/OperationFilterContributor.cs
@@ -1,37 +1,34 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using OpenRasta.DI;
 using OpenRasta.OperationModel;
-using OpenRasta.OperationModel.Filters;
-using OpenRasta.Pipeline;
 using OpenRasta.Web;
 
 namespace OpenRasta.Pipeline.Contributors
 {
   public class OperationFilterContributor : KnownStages.IOperationFiltering
   {
-    private readonly Func<IEnumerable<IOperationFilter>> _createFilters;
+    readonly Func<IEnumerable<IOperationFilter>> _createFilters;
 
-    public OperationFilterContributor(IDependencyResolver resolver)
+    public OperationFilterContributor(Func<IEnumerable<IOperationFilter>> createFilters)
     {
-      _createFilters = ()=>resolver.ResolveAll<IOperationFilter>();
+      _createFilters = createFilters;
     }
 
     PipelineContinuation ProcessOperations(ICommunicationContext context)
     {
       var ops = ProcessOperations(context.PipelineData.OperationsAsync.ToList());
       context.PipelineData.OperationsAsync = ops;
-      
+
       return !ops.Any()
         ? OnOperationsEmpty(context)
         : PipelineContinuation.Continue;
     }
 
-    private List<IOperationAsync> ProcessOperations(List<IOperationAsync> operations)
+    List<IOperationAsync> ProcessOperations(List<IOperationAsync> operations)
     {
       return _createFilters()
-       .Aggregate(
+        .Aggregate(
           operations,
           (ops, filter) => filter.Process(ops).ToList())
         .Distinct()
--- src/OpenRasta/Pipeline/Contributors/OperationInvokerContributor.cs
+++ src/OpenRasta/Pipeline/Contributors/OperationInvokerContributor.cs
@@ -1,20 +1,18 @@
 ﻿using System;
 using System.Threading.Tasks;
-using OpenRasta.DI;
 using OpenRasta.OperationModel;
 using OpenRasta.OperationModel.Interceptors;
 using OpenRasta.Web;
-using OpenRasta.Pipeline;
 
 namespace OpenRasta.Pipeline.Contributors
 {
   public class OperationInvokerContributor : KnownStages.IOperationExecution
   {
-    readonly IDependencyResolver _resolver;
+    readonly Func<IOperationExecutor> _executorFactory;
 
-    public OperationInvokerContributor(IDependencyResolver resolver)
+    public OperationInvokerContributor(Func<IOperationExecutor> executorFactory)
     {
-      _resolver = resolver;
+      _executorFactory = executorFactory;
     }
 
     public void Initialize(IPipeline pipelineRunner)
@@ -24,7 +22,7 @@ namespace OpenRasta.Pipeline.Contributors
 
     async Task<PipelineContinuation> ExecuteOperations(ICommunicationContext context)
     {
-      var executor = _resolver.Resolve<IOperationExecutor>();
+      var executor = _executorFactory();
       try
       {
         context.OperationResult = await executor.Execute(context.PipelineData.OperationsAsync);
