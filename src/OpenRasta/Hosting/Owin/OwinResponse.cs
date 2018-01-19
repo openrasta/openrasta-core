fc17244f92e18c645b6f8e802afa3d6c024fd082
Sebastien Lambla <seb@serialseb.com>
2018-01-19 22:58:32 +0100

    Remove owin dependency on DM

--- src/OpenRasta/Hosting/Owin/OwinResponse.cs
+++ src/OpenRasta/Hosting/Owin/OwinResponse.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using LibOwin;
-
 using OpenRasta.DI;
 using OpenRasta.Web;
 
@@ -35,23 +34,14 @@ namespace OpenRasta.Hosting.Katana
     {
       if (HeadersSent)
         throw new InvalidOperationException("The headers have already been sent.");
-      foreach (var header in Headers.Where(h => h.Key != "Content-Type"))
-        try
-        {
-          NativeContext.Headers.Remove(header.Key);
-          NativeContext.Headers.Append(header.Key, header.Value);
-        }
-        catch (Exception ex)
-        {
-          var commcontext = DependencyManager.GetService<ICommunicationContext>();
-          commcontext?.ServerErrors.Add(new Error {Message = ex.ToString()});
-        }
+      
+      foreach (var header in Headers)
+      {
+        NativeContext.Headers.Remove(header.Key);
+        NativeContext.Headers.Append(header.Key, header.Value);
+      }
 
       HeadersSent = true;
-      if (Headers.ContentType != null)
-        NativeContext.Headers.Add(new KeyValuePair<string, string[]>("Content-Type",
-          new[] {Headers.ContentType.MediaType}));
-
       Entity.Stream.Flush();
     }
   }
