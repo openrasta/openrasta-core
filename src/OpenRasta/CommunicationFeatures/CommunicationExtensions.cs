d44f7456a55fa53043135cafb6528b3cd8d58d28
Sebastien Lambla <seb@serialseb.com>
2018-02-27 11:55:01 +0100

    Add ICommunicationContext.Feature

---
+++ src/OpenRasta/CommunicationFeatures/CommunicationExtensions.cs
@@ -0,0 +1,28 @@
+﻿using OpenRasta.Web;
+
+namespace OpenRasta.CommunicationFeatures
+{
+  public static class CommunicationExtensions
+  {
+    public static void Feature<T>(this ICommunicationContext context, T feature)
+    {
+      context.PipelineData[GetFeatureKey<T>()] = feature;
+    }
+    public static bool Feature<T>(this ICommunicationContext context, out T feature) where T : class
+    {
+      if (!context.PipelineData.TryGetValue(GetFeatureKey<T>(), out var untypedFeature))
+      {
+        feature = null;
+        return false;
+      }
+
+      feature = (T) untypedFeature;
+      return true;
+    }
+
+    static string GetFeatureKey<T>()
+    {
+      return $"openrasta.{typeof(T).Name}";
+    }
+  }
+}
