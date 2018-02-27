830eaa49898c573ab0b34129a09831404fb27e75
Sebastien Lambla <seb@serialseb.com>
2018-02-27 17:09:12 +0100

    Only mess with Length if there is a Length

--- src/OpenRasta/Hosting/HttpListener/HttpListenerResponse.cs
+++ src/OpenRasta/Hosting/HttpListener/HttpListenerResponse.cs
@@ -16,24 +16,23 @@ namespace OpenRasta.Hosting.HttpListener
       _context = context;
       _nativeResponse = response;
       Headers = new HttpHeaderDictionary();
-      // TODO: Wrap stream and send chunked when needed if write starts before sending response
       Entity = new HttpEntity(Headers, response.OutputStream);
     }
 
     public IHttpEntity Entity { get; set; }
-    public HttpHeaderDictionary Headers { get; private set; }
+    public HttpHeaderDictionary Headers { get; }
     public bool HeadersSent { get; private set; }
 
     public int StatusCode
     {
-      get { return _nativeResponse.StatusCode; }
-      set { _nativeResponse.StatusCode = value; }
+      get => _nativeResponse.StatusCode;
+      set => _nativeResponse.StatusCode = value;
     }
 
     public void WriteHeaders()
     {
-      if (HeadersSent)
-        throw new InvalidOperationException("The headers have already been sent.");
+      if (HeadersSent) return;
+
       HeadersSent = true;
       _nativeResponse.Headers.Clear();
       foreach (var header in Headers.Where(h => h.Key != "Content-Length"))
@@ -48,7 +47,8 @@ namespace OpenRasta.Hosting.HttpListener
         }
       }
 
-      _nativeResponse.ContentLength64 = Headers.ContentLength.GetValueOrDefault();
+      if (Headers.ContentLength != null)
+        _nativeResponse.ContentLength64 = Headers.ContentLength.Value;
     }
   }
 }
