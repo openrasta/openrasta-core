0e2aa6349b791a77d3b3e24aa5d72afb95c2c225
Sebastien Lambla <seb@serialseb.com>
2018-03-17 05:27:36 +0000

    Making the corefx repro minimal

---
+++ src/Tests/Plugins.ReverseProxy/corefx_repro.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using OpenRasta.Plugins.ReverseProxy;
+using Tests.Hosting.Owin;
+using Xunit;
+
+namespace Tests.Plugins.ReverseProxy
+{
+  #if NETCOREAPP2_0
+  public class corefx_repro
+  {
+    [Fact]
+    public async Task maybe_itll_fail_i_dont_even()
+    {
+      var toServer =
+        new WebHostBuilder()
+          .UseKestrel()
+          .UseUrls("http://127.0.0.1:0")
+          .Configure(app => { app.Run(async context => await context.Response.WriteAsync("hello")); })
+          .Build();
+      toServer.Start();
+      var toPort = toServer.Port();
+      var rpClient = new ReproReverseProxy();
+      var fromServer=new WebHostBuilder()
+        .Configure(app =>
+        {
+          app.Run(async context =>
+          {
+            var targetResponse = await rpClient.Send(context, $"http://127.0.0.1:{toPort}");
+            await ReproCodec.Write(context, targetResponse);
+          });
+        })
+        .UseKestrel()
+        .UseUrls("http://127.0.0.1:0")
+        .Build();
+      fromServer.Start();
+
+      var client = new HttpClient();
+      var response = await client.GetAsync($"http://127.0.0.1:{fromServer.Port()}");
+      response.EnsureSuccessStatusCode();
+      
+      client.Dispose();
+      toServer.Dispose();
+      fromServer.Dispose();
+    }
+  }
+  
+  public class ReproCodec
+  {
+    public static async Task Write(HttpContext context, ReverseProxyResponse proxyResponse)
+    {
+      try
+      {
+        context.Response.StatusCode = proxyResponse.StatusCode;
+
+        if (proxyResponse.ResponseMessage != null)
+        {
+          await proxyResponse.ResponseMessage.Content.CopyToAsync(context.Response.Body);
+          context.Response.Body.Flush();
+        }
+      }
+      finally
+      {
+        proxyResponse.Dispose();
+      }
+    }
+  }
+  public class ReproReverseProxy
+  {
+    public async Task<ReverseProxyResponse> Send(HttpContext context, string target)
+    {
+      var httpClient = new HttpClient(new HttpClientHandler());
+      var requestMessage = new HttpRequestMessage
+      {
+        Method = new HttpMethod(context.Request.Method),
+        Content = new StreamContent(context.Request.Body)
+      };
+
+      CopyHeaders(context, requestMessage);
+
+      var headers = requestMessage.Headers;
+      
+      requestMessage.RequestUri = new Uri(target);
+      try
+      {
+        var responseMessage = await httpClient.SendAsync(
+          requestMessage,
+          HttpCompletionOption.ResponseHeadersRead
+        );
+        return new ReverseProxyResponse(requestMessage, responseMessage);
+      }
+      catch (TaskCanceledException e)
+      {
+        return new ReverseProxyResponse(requestMessage, error: e, statusCode: 504);
+      }
+    }
+
+    static void CopyHeaders(HttpContext context, HttpRequestMessage request)
+    {
+      foreach (var header in context.Request.Headers)
+      {
+        if (header.Key.Equals("host", StringComparison.OrdinalIgnoreCase)) continue;
+        if (header.Key.StartsWith("Content-"))
+        {
+          request.Content.Headers.Add(header.Key, header.Value.ToList());
+        }
+        else
+          request.Headers.Add(header.Key, header.Value.ToList());
+      }
+    }
+  }
+  #endif
+}
--- src/Tests/Plugins.ReverseProxy/get_returning_200_on_kestrel.cs
+++ src/Tests/Plugins.ReverseProxy/get_returning_200_on_kestrel.cs
@@ -7,14 +7,11 @@ using System.Net.Http.Headers;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using OpenRasta;
 using OpenRasta.Plugins.ReverseProxy;
 using OpenRasta.Web;
 using Shouldly;
-using Tests.Hosting.Owin;
 using Tests.Plugins.ReverseProxy.Implementation;
 using Xunit;
 using HttpMethod = System.Net.Http.HttpMethod;
@@ -36,129 +33,6 @@ namespace Tests.Plugins.ReverseProxy
         response.Message.StatusCode.ShouldBe(HttpStatusCode.OK);
         response.Content.ShouldEndWith("/proxied");
       }
-    }
-  }
-
-  public class corefx_repro
-  {
-    [Fact]
-    public async Task maybe_itll_fail_i_dont_even()
-    {
-      var toServer =
-        new WebHostBuilder()
-          .UseKestrel()
-          .UseUrls("http://127.0.0.1:0")
-          .Configure(app => { app.Run(async context => await context.Response.WriteAsync("hello")); })
-          .Build();
-      toServer.Start();
-      var toPort = toServer.Port();
-      var rpClient = new ReproReverseProxy(new ReverseProxyOptions());
-      var fromServer=new WebHostBuilder()
-        .Configure(app =>
-        {
-          app.Run(async context =>
-          {
-            var targetResponse = await rpClient.Send(context, $"http://127.0.0.1:{toPort}");
-            await ReproCodec.Write(context, targetResponse);
-          });
-        })
-        .UseKestrel()
-        .UseUrls("http://127.0.0.1:0")
-        .Build();
-      fromServer.Start();
-
-      var client = new HttpClient();
-      var response = await client.GetAsync($"http://127.0.0.1:{fromServer.Port()}");
-      response.EnsureSuccessStatusCode();
-    }
-  }
-
-  public class ReproCodec
-  {
-    public static async Task Write(HttpContext context, ReverseProxyResponse proxyResponse)
-    {
-      
-      try
-      {
-        context.Response.StatusCode = proxyResponse.StatusCode;
-
-        if (proxyResponse.ResponseMessage != null)
-        {
-//          foreach (var header in proxyResponse.ResponseMessage.Headers)
-//            context.Response.Headers.Add(header.Key, header.Value.ToArray());
-
-          await proxyResponse.ResponseMessage.Content.CopyToAsync(context.Response.Body);
-          context.Response.Body.Flush();
-        }
-      }
-      finally
-      {
-        proxyResponse.Dispose();
-      }
-    }
-  }
-  public class ReproReverseProxy
-  {
-    readonly ReverseProxyOptions _options;
-    readonly Lazy<HttpClient> _httpClient;
-    readonly TimeSpan _timeout;
-
-    public ReproReverseProxy(ReverseProxyOptions options)
-    {
-      _options = options;
-      _timeout = options.Timeout;
-      _httpClient = new Lazy<HttpClient>(() => new HttpClient(_options.HttpMessageHandler())
-        {
-          Timeout = Timeout.InfiniteTimeSpan
-        },
-        LazyThreadSafetyMode.ExecutionAndPublication);
-    }
-
-    public async Task<ReverseProxyResponse> Send(HttpContext context, string target)
-    {
-      var requestMessage = new HttpRequestMessage
-      {
-        Method = new HttpMethod(context.Request.Method),
-        Content = new StreamContent(context.Request.Body)
-      };
-
-      CopyHeaders(context, requestMessage, _options.FrowardedHeaders.ConvertLegacyHeaders);
-
-      var headers = requestMessage.Headers;
-      var identifier = _options.Via.Pseudonym ?? "test";
-      headers.Add("via", $"1.1 {identifier}");
-
-      requestMessage.RequestUri = new Uri(target);
-      var cts = new CancellationTokenSource();
-      cts.CancelAfter(_timeout);
-      var token = cts.Token;
-      try
-      {
-        var responseMessage = await _httpClient.Value.SendAsync(
-          requestMessage,
-          HttpCompletionOption.ResponseHeadersRead,
-          token
-        );
-        responseMessage.Headers.Via.Add(new ViaHeaderValue("1.1", identifier));
-        return new ReverseProxyResponse(requestMessage, responseMessage);
-      }
-      catch (TaskCanceledException e) when (token.IsCancellationRequested || e.CancellationToken == token)
-      {
-        return new ReverseProxyResponse(requestMessage, error: e, statusCode: 504);
-      }
-    }
-
-    static void CopyHeaders(HttpContext context, HttpRequestMessage request, bool convertLegacyHeaders)
-    {
-      foreach (var header in context.Request.Headers)
-      {
-        if (header.Key.Equals("host", StringComparison.OrdinalIgnoreCase)) continue;
-        if (header.Key.StartsWith("Content-"))
-          request.Content.Headers.Add(header.Key, header.Value.ToList());
-        else
-          request.Headers.Add(header.Key, header.Value.ToList());
-      }
-
     }
   }
 #endif
