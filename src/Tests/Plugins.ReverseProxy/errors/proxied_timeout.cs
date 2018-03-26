dfba6dff39541e70aa49bbd0735879845fe4dd96
Sebastien Lambla <seb@serialseb.com>
2018-03-26 10:20:02 +0200

    Make the test actually timeout

--- src/Tests/Plugins.ReverseProxy/errors/proxied_timeout.cs
+++ src/Tests/Plugins.ReverseProxy/errors/proxied_timeout.cs
@@ -14,10 +14,10 @@ namespace Tests.Plugins.ReverseProxy.errors
     public async Task response_is_gateway_timeout()
     {
       using (var response = await new ProxyServer()
-        .FromServer(port => $"http://127.0.0.1:{port}/proxy", options => options.Timeout = TimeSpan.FromSeconds(30))
+        .FromServer(port => $"http://127.0.0.1:{port}/proxy", options => options.Timeout = TimeSpan.FromSeconds(1))
         .ToServer(port => $"http://127.0.0.1:{port}/proxied", async ctx =>
         {
-          await Task.Delay(TimeSpan.FromSeconds(1));
+          await Task.Delay(TimeSpan.FromSeconds(30));
           return "OK";
         })
         .UseKestrel()
