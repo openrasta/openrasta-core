bb4796c464dbb5886433eace55042edda1fa7361
Toby Henderson <toby.henderson@huddle.com>
2018-03-31 18:28:34 +0100

    Updates to new project format and starts version 2.6

--- src/OpenRasta.Hosting.AspNet/Properties/AssemblyInfo.cs
+++ src/OpenRasta.Hosting.AspNet/Properties/AssemblyInfo.cs
@@ -1,7 +1,4 @@
-using System.Reflection;
 using System.Web;
 using OpenRasta.Hosting.AspNet;
 
 [assembly: PreApplicationStartMethod(typeof(OpenRastaModuleLoader), nameof(OpenRastaModuleLoader.Load))]
-[assembly: AssemblyTitle("OpenRasta asp.net Support")]
-[assembly: AssemblyDescription("Support for IIS / asp.net hosting of OpenRasta.")]
