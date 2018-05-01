5f7e093c7d5796ccf61201fdeb9ec8501036af5a
Sebastien Lambla <seb@serialseb.com>
2018-05-01 18:48:21 +0200

    handler that locks on IPAddress

---
+++ src/OpenRasta.Plugins.ReverseProxy/LoadBalancingHttpClientFactory/LockToIPAddress.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OpenRasta.Plugins.ReverseProxy.LoadBalancingHttpClientFactory
+{
+  public class LockToIPAddress : DelegatingHandler
+  {
+    readonly Func<string, Task<IPAddress>> _dnsResolver;
+    readonly SemaphoreSlim _dnsResolverLock = new SemaphoreSlim(1);
+    IPAddress _ipAddress;
+    string _rewrittenHost;
+
+    public LockToIPAddress(HttpMessageHandler inner, Func<string,Task<IPAddress>> dnsResolver) : base(inner)
+    {
+      _dnsResolver = dnsResolver;
+    }
+
+    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+      var host = request.RequestUri.Host;
+      await EnsureLockOnIPAddress(request, cancellationToken, host);
+
+      if (string.Equals(host, _rewrittenHost, StringComparison.OrdinalIgnoreCase))
+      {
+        request.RequestUri = new UriBuilder(request.RequestUri) {Host = _ipAddress.ToString()}.Uri;
+        request.Headers.Host = host;
+      }
+      
+      return await base.SendAsync(request, cancellationToken);
+    }
+
+    async Task EnsureLockOnIPAddress(HttpRequestMessage request, CancellationToken cancellationToken, string host)
+    {
+      if (_ipAddress == null)
+      {
+        await _dnsResolverLock.WaitAsync(cancellationToken);
+        try
+        {
+          if (_ipAddress == null)
+          {
+            _rewrittenHost = request.RequestUri.Host;
+            if (IPAddress.TryParse(host, out var ip))
+              _ipAddress = ip;
+            else
+              _ipAddress = await _dnsResolver(host);
+          }
+        }
+        finally
+        {
+          _dnsResolverLock.Release();
+        }
+      }
+    }
+  }
+}
---
+++ src/Tests/Plugins.ReverseProxy.LoadBalancingHttpClientFactory/locking_to_dns_record.cs
@@ -0,0 +1,67 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using OpenRasta.Plugins.ReverseProxy.LoadBalancingHttpClientFactory;
+using Shouldly;
+using Xunit;
+
+namespace Tests.Plugins.ReverseProxy.LoadBalancingHttpClientFactory
+{
+  public class locking_to_dns_record
+  {
+    ResponseCapturingHttpMessageHandler simulator;
+    HttpClient client;
+
+    public locking_to_dns_record()
+    {
+      Task<IPAddress> resolver(string domainName) => Task.FromResult(IPAddress.Parse("127.0.0.1"));
+
+      simulator = new ResponseCapturingHttpMessageHandler();
+      var delegateHandler = new LockToDnsRecordA(simulator, resolver);
+      client = new HttpClient(delegateHandler);
+    }
+    [Fact]
+    public async Task request_uri_is_rewritten()
+    {
+      await client.GetAsync("http://localhost/path");
+      simulator.Request.RequestUri.ToString().ShouldBe("http://127.0.0.1/path");
+    }
+    [Fact]
+    public async Task host_header_is_original_host()
+    {
+      await client.GetAsync("http://localhost/path");
+      simulator.Request.Headers.Host.ShouldBe("localhost");
+    }
+
+    [Fact]
+    public async Task only_first_host_gets_rewritten()
+    {
+      await client.GetAsync("http://localhost/path");
+      simulator.Request.RequestUri.ToString().ShouldBe("http://127.0.0.1/path");
+      
+      await client.GetAsync("http://google.com/path");
+      simulator.Request.RequestUri.ToString().ShouldBe("http://google.com/path");
+    }
+
+    [Fact]
+    public async Task ip_address_is_not_rewritten()
+    {
+      await client.GetAsync("http://192.168.0.1/path");
+      simulator.Request.RequestUri.ToString().ShouldBe("http://192.168.0.1/path");
+      simulator.Request.Headers.Host.ShouldBe("192.168.0.1");
+    }
+  }
+
+  class ResponseCapturingHttpMessageHandler : HttpMessageHandler
+  {
+    protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request,
+      CancellationToken cancellationToken)
+    {
+      Request = request;
+      return Task.FromResult(new HttpResponseMessage());
+    }
+
+    public HttpRequestMessage Request { get; set; }
+  }
+}
