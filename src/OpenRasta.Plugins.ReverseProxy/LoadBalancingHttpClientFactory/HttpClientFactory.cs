80c9983d8d63750e1344527724ab2fe221907299
Sebastien Lambla <seb@serialseb.com>
2018-05-01 11:33:21 +0200

    Initial implementation

---
+++ src/OpenRasta.Plugins.ReverseProxy/LoadBalancingHttpClientFactory/HttpClientFactory.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+
+namespace OpenRasta.Plugins.ReverseProxy.LoadBalancingHttpClientFactory
+{
+  public class HttpClientFactory
+  {
+    readonly int _count;
+    readonly Func<HttpMessageHandler> _handlerFactory;
+    readonly TimeSpan _clientLeaseTime;
+    readonly ConcurrentDictionary<int, ActiveHandler> _handlers;
+    readonly ConcurrentQueue<EvictedHandler> _evictedHandlers;
+
+    int _index;
+
+    public HttpClientFactory(int count, Func<HttpMessageHandler> handlerFactory, TimeSpan clientLeaseTime)
+    {
+      _count = count;
+      _handlerFactory = handlerFactory;
+      _clientLeaseTime = clientLeaseTime;
+      _handlers = new ConcurrentDictionary<int, ActiveHandler>();
+      _evictedHandlers = new ConcurrentQueue<EvictedHandler>();
+      _cleanupTimer = new Timer(state => Cleanup(), null, Timeout.InfiniteTimeSpan, Timeout.InfiniteTimeSpan);
+    }
+
+    public HttpClient GetClient()
+    {
+      var idx = Interlocked.Increment(ref _index);
+      var clientKey = idx > 0 ? idx % _count : idx * -1 % _count;
+
+      return new HttpClient(_handlers.GetOrAdd(clientKey, CreateActiveHandler), disposeHandler: false);
+    }
+
+    ActiveHandler CreateActiveHandler(int position)
+    {
+      return new ActiveHandler(position, _handlerFactory(), _clientLeaseTime, EvictHandler);
+    }
+
+    void EvictHandler(ActiveHandler handler)
+    {
+      if (!_handlers.TryRemove(handler.Index, out _)) throw new InvalidOperationException("Could not remove handler");
+
+      _evictedHandlers.Enqueue(new EvictedHandler(handler));
+      ScheduleCleanup();
+    }
+
+    void Cleanup()
+    {
+      _cleanupScheduled = 0;
+
+      var unready = new List<EvictedHandler>();
+
+      while (_evictedHandlers.TryDequeue(out var handler))
+      {
+        if (handler.IsDisposable) handler.Dispose();
+        else unready.Add(handler);
+      }
+
+      if (!unready.Any()) return;
+
+      foreach (var handler in unready)
+        _evictedHandlers.Enqueue(handler);
+
+      ScheduleCleanup();
+    }
+
+    readonly Timer _cleanupTimer;
+    int _cleanupScheduled = 0;
+
+    void ScheduleCleanup()
+    {
+      if (Interlocked.Exchange(ref _cleanupScheduled, 1) == 0)
+        _cleanupTimer.Change(TimeSpan.FromSeconds(1), Timeout.InfiniteTimeSpan);
+    }
+  }
+
+  class ActiveHandler : DelegatingHandler
+  {
+    Action<ActiveHandler> _evict;
+    public int Index { get; }
+    Timer _evictionTimer;
+
+    public ActiveHandler(int index, HttpMessageHandler innerHandler, TimeSpan clientLeaseTime,
+      Action<ActiveHandler> evict) : base(innerHandler)
+    {
+      _evict = evict;
+      Index = index;
+      _evictionTimer = new Timer(Evict, evict, clientLeaseTime, Timeout.InfiniteTimeSpan);
+    }
+
+    void Evict(object state)
+    {
+      _evictionTimer.Dispose();
+      _evictionTimer = null;
+      _evict(this);
+      _evict = null;
+    }
+  }
+
+  class EvictedHandler
+  {
+    readonly WeakReference _weakReference;
+
+    public EvictedHandler(ActiveHandler handler)
+    {
+      _disposableHandler = handler.InnerHandler;
+      _weakReference = new WeakReference(handler);
+    }
+
+    readonly HttpMessageHandler _disposableHandler;
+    public bool IsDisposable => !_weakReference.IsAlive;
+
+    public void Dispose()
+    {
+      _disposableHandler.Dispose();
+    }
+  }
+}
---
+++ src/Tests/Plugins.ReverseProxy.LoadBalancingHttpClientFactory/getting_clients.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using OpenRasta.Plugins.ReverseProxy.LoadBalancingHttpClientFactory;
+using Shouldly;
+using Xunit;
+
+namespace Tests.Plugins.ReverseProxy.LoadBalancingHttpClientFactory
+{
+  public class getting_clients
+  {
+    [Fact]
+    public void creates_as_many_handlers_as_requested_count()
+    {
+      var createdHandlers = new List<HttpMessageHandler>();
+
+      HttpMessageHandler createHandler()
+      {
+        var handler = new NullHandler();
+        createdHandlers.Add(handler);
+        return handler;
+      }
+
+      var factory = new HttpClientFactory(5, createHandler, Timeout.InfiniteTimeSpan);
+
+      var clients = Enumerable.Range(0, 10).Select(i => factory.GetClient()).ToList();
+      clients.Distinct().Count().ShouldBe(10);
+      createdHandlers.Count.ShouldBe(5);
+    }
+
+    [Fact]
+    public async Task handler_is_evicted()
+    {
+      var createdHandlers = new List<HttpMessageHandler>();
+
+      HttpMessageHandler createHandler()
+      {
+        var handler = new NullHandler();
+        createdHandlers.Add(handler);
+        return handler;
+      }
+
+      var factory = new HttpClientFactory(1, createHandler, TimeSpan.FromSeconds(5));
+
+      var clients = Enumerable.Range(0, 10).Select(i => factory.GetClient()).ToList();;
+      createdHandlers.Count.ShouldBe(1);
+      await Task.Delay(TimeSpan.FromSeconds(5));
+      clients = Enumerable.Range(0, 10).Select(i => factory.GetClient()).ToList();;
+      createdHandlers.Count.ShouldBe(2);
+    }
+    [Fact]
+    public async Task handler_is_disposed()
+    {
+      NullHandler handler = null;
+      HttpMessageHandler createHandler()
+      {
+        return handler = new NullHandler();
+      }
+
+      var factory = new HttpClientFactory(1, createHandler, TimeSpan.FromMilliseconds(1));
+
+      using(var client = factory.GetClient()){}
+
+      var sw = Stopwatch.StartNew();
+
+      TimeSpan timeout = TimeSpan.FromSeconds(15);
+      do
+      {
+        GC.Collect(2, GCCollectionMode.Forced, true, true);
+        GC.WaitForPendingFinalizers();
+        GC.Collect(2, GCCollectionMode.Forced, true, true);
+      } while (!handler.IsDisposed && sw.Elapsed < timeout);
+      
+      handler.IsDisposed.ShouldBeTrue();
+    }
+  }
+
+  class NullHandler : HttpMessageHandler
+  {
+    protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+      return Task.FromResult(new HttpResponseMessage());
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+      base.Dispose(disposing);
+      IsDisposed = true;
+    }
+
+    public bool IsDisposed { get; set; }
+  }
+}
