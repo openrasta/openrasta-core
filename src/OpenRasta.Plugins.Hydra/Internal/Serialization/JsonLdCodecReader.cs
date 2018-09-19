ff0d59eb205684d9eb3fd2650f5d9eb39b999d17
Jonathan Channon <jonathan.channon@gmail.com>
2018-09-19 14:34:56 +0100

    created jsonnet metamodelhandler

---
+++ src/OpenRasta.Plugins.Hydra/Internal/Serialization/JsonLdCodecReader.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using OpenRasta.Codecs;
+using OpenRasta.TypeSystem;
+using OpenRasta.Web;
+
+namespace OpenRasta.Plugins.Hydra.Internal.Serialization
+{
+  public class JsonLdCodecReader : IMediaTypeReaderAsync
+  {
+    public object Configuration { get; set; }
+
+    public async Task<object> ReadFrom(IHttpEntity request, IType destinationType, string destinationName)
+    {
+      var content = await new StreamReader(request.Stream, Encoding.UTF8).ReadToEndAsync();
+
+      return JsonConvert.DeserializeObject(content, destinationType.StaticType);
+    }
+  }
+}
