797593b2b01390a747bf7ee2a90bba9bf763e3cb
Jonathan Channon <jonathan.channon@gmail.com>
2018-10-08 22:06:50 +0100

    Add extra info into ClassDefinition

--- src/OpenRasta.Plugins.Hydra/Internal/Serialization/JsonNet/ClassDefinitionHandler.cs
+++ src/OpenRasta.Plugins.Hydra/Internal/Serialization/JsonNet/ClassDefinitionHandler.cs
@@ -1,3 +1,8 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
 using OpenRasta.Configuration.MetaModel;
 using OpenRasta.Configuration.MetaModel.Handlers;
 
@@ -7,7 +12,6 @@ namespace OpenRasta.Plugins.Hydra.Internal
   {
     public ClassDefinitionHandler()
     {
-      
     }
 
     public void PreProcess(IMetaModelRepository repository)
@@ -16,10 +20,84 @@ namespace OpenRasta.Plugins.Hydra.Internal
 
     public void Process(IMetaModelRepository repository)
     {
-      foreach (var resourceRegistration in repository.ResourceRegistrations)
+      foreach (var resourceRegistration in repository.ResourceRegistrations.Where(x => x.ResourceType.Name == "Event"))
       {
-        resourceRegistration.ClassDefinition = new ClassDefinition(resourceRegistration.ResourceType.GetProperties(), resourceRegistration.ResourceType.Name);
+        var classProperties = new List<ClassProperty>();
+        var properties = resourceRegistration.ResourceType.GetProperties();
+
+        foreach (var property in properties)
+        {
+          /*
+           
+           Check for json.net attributes first. If they use a mixture of attribute types then
+           
+              )
+             (
+            .-`-.
+            :   :
+            :TNT:
+            :___:
+           
+           */
+
+          if (property.CustomAttributes.Any(x => x.AttributeType == typeof(JsonIgnoreAttribute) || x.AttributeType == typeof(IgnoreDataMemberAttribute)))
+          {
+            continue;
+          }
+
+          string propertyName = string.Empty;
+
+          if (property.CustomAttributes.Any(x => x.AttributeType == typeof(JsonPropertyAttribute)))
+          {
+            propertyName = property.CustomAttributes.Where(x => x.AttributeType == typeof(JsonPropertyAttribute)).Select(x => x.ConstructorArguments.FirstOrDefault().Value.ToString())
+              .FirstOrDefault();
+          }
+          else if (property.CustomAttributes.Any(x => x.AttributeType == typeof(DataMemberAttribute)))
+          {
+            propertyName = property.CustomAttributes.Where(x => x.AttributeType == typeof(DataMemberAttribute)).SelectMany(x => x.NamedArguments).Where(c => c.MemberName == "Name")
+              .Select(v => v.TypedValue.Value.ToString()).FirstOrDefault();
+          }
+
+          if (string.IsNullOrWhiteSpace(propertyName))
+          {
+            propertyName = MakeCamelCase(property.Name);
+          }
+
+          //Should we explicitly check for Collection type (i.e. HydraCollection)
+
+          Type propertyType = null;
+
+          if (property.PropertyType.GetGenericArguments().Any())
+          {
+            propertyType = property.PropertyType.GetGenericArguments()[0];
+          }
+
+          propertyType = propertyType ?? property.PropertyType;
+
+          if (repository.ResourceRegistrations.Any(x => x.ResourceType == propertyType))
+          {
+            classProperties.Add(new ClassProperty(propertyName, repository.ResourceRegistrations.FirstOrDefault(x => x.ResourceType == propertyType)));
+          }
+          else
+          {
+            classProperties.Add(new ClassProperty(propertyName));
+          }
+        }
+
+        //We need to store the Vocabulary but if we pass that to ClassDefinition we leak Hydra into OR.Core so do we pass a Dictionary<string,object> to ClassDefinition for plugin specific properties?
+
+        resourceRegistration.ClassDefinition = new ClassDefinition(classProperties, resourceRegistration.ResourceType.Name, resourceRegistration);
       }
+    }
+
+    public static string MakeCamelCase(string name)
+    {
+      if (char.IsLower(name[0]))
+      {
+        return name;
+      }
+
+      return string.Concat(char.ToLowerInvariant(name[0]), name.Substring(1));
     }
   }
 }
