040e09b55a6866c632472edef871b0d05c4eaa42
Jonathan Channon <jonathan.channon@gmail.com>
2018-10-09 18:52:19 +0100

    Added property type to ClassProperty Removed check on actual Json types to remove lib dependency Made inline if checks not have brackets

--- src/OpenRasta/Configuration/MetaModel/ClassDefinition.cs
+++ src/OpenRasta/Configuration/MetaModel/ClassDefinition.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace OpenRasta.Configuration.MetaModel
 {
@@ -20,13 +21,20 @@ namespace OpenRasta.Configuration.MetaModel
 
   public class ClassProperty
   {
-    public ClassProperty(string name) => Name = name;
-
-    public ClassProperty(string name, ResourceModel resourceModel)
+    public ClassProperty(string name, Type propertyType)
     {
       Name = name;
+      PropertyType = propertyType;
+    }
+
+    public ClassProperty(string name, Type propertyType, ResourceModel resourceModel)
+    {
+      Name = name;
+      PropertyType = propertyType;
       ResourceModel = resourceModel;
     }
+
+    public Type PropertyType { get; set; }
 
     public ResourceModel ResourceModel { get; set; }
 
