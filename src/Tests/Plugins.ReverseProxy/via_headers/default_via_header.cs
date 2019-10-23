718d232b8c6b0d0c9c05bf7d2d1d44babb1744c4
Sebastien Lambla <seb@serialseb.com>
2019-10-23 14:38:59 +0200

    Fix other test for varying protocol name

--- src/Tests/Plugins.ReverseProxy/via_headers/default_via_header.cs
+++ src/Tests/Plugins.ReverseProxy/via_headers/default_via_header.cs
@@ -14,7 +14,7 @@ namespace Tests.Plugins.ReverseProxy.via_headers
         .FromServer("/proxy")
         .ToServer("/proxied", async ctx => ctx.Request.Headers["Via"])
         .GetAsync("http://source.example/proxy");
-      response.Content.ShouldBe("HTTP/2.0 source.example:80");
+      response.Content.ShouldMatch(@"\S+ source.example:80");
 
       var via = response.Message.Headers.Via.ShouldHaveSingleItem();
       via.ReceivedBy.ShouldBe("source.example:80");
