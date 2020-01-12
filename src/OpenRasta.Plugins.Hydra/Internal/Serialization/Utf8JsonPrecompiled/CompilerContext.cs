b0956e9c01ea9c383efd80ebf6143bbc51213c8b
Sebastien Lambla <seb@serialseb.com>
2020-01-12 15:16:19 +0100

    Expand CompilerContext

--- src/OpenRasta.Plugins.Hydra/Internal/Serialization/Utf8JsonPrecompiled/CompilerContext.cs
+++ src/OpenRasta.Plugins.Hydra/Internal/Serialization/Utf8JsonPrecompiled/CompilerContext.cs
@@ -1,16 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using OpenRasta.Configuration.MetaModel;
 
 namespace OpenRasta.Plugins.Hydra.Internal.Serialization.Utf8JsonPrecompiled
 {
   public class CompilerContext
   {
+    List<ResourceModel> _recursionDefender = new List<ResourceModel>();
+
     public CompilerContext(IMetaModelRepository metaModel, ResourceModel resource)
     {
       MetaModel = metaModel;
       Resource = resource;
     }
 
-    public IMetaModelRepository MetaModel { get; set; }
-    public ResourceModel Resource { get; set; }
+    public IMetaModelRepository MetaModel { get; }
+    public ResourceModel Resource { get; }
+
+    public CompilerContext Push(ResourceModel resourceModel)
+    {
+      if (_recursionDefender.Any(m => m.ResourceType == resourceModel.ResourceType))
+        throw new InvalidOperationException(
+          $"Detected recursion, already processing {resourceModel.ResourceType?.Name}: {string.Join("->", _recursionDefender.Select(m => m.ResourceType?.Name).Where(n => n != null))}");
+      return new CompilerContext(MetaModel, resourceModel)
+      {
+        _recursionDefender = _recursionDefender.Concat(new[]{resourceModel}).ToList()
+      };
+    }
   }
 }
