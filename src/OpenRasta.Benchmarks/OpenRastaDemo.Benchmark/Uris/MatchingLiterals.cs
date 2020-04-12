e45c6e00035bf1f91ae9b03ec7059b3fa58371dd
Sebastien Lambla <seb@serialseb.com>
2020-04-13 01:31:09 +0200

    Benchmark for URI parsing

---
+++ src/OpenRasta.Benchmarks/OpenRastaDemo.Benchmark/Uris/MatchingLiterals.cs
@@ -0,0 +1,41 @@
+using System;
+using BenchmarkDotNet.Attributes;
+using OpenRasta.Web;
+using Xunit;
+
+namespace OpenRastaDemo.Benchmark.Uris
+{
+  [HtmlExporter, InProcess, MemoryDiagnoser]
+  public class Matching
+  {
+    TemplatedUriResolver _templatedUriResolver;
+
+    public Matching()
+    {
+      _templatedUriResolver = new TemplatedUriResolver()
+      {
+        new UriRegistration("/events/", new object()),
+        new UriRegistration("/events/{id}", new object()),
+        new UriRegistration("/events/?by={by}", new object())        
+      };
+    }
+
+    [Benchmark]
+    public void Literal()
+    {
+      _templatedUriResolver.Match(new Uri("https://localhost/events/"));
+    }
+
+    [Benchmark]
+    public void Segment()
+    {
+      _templatedUriResolver.Match(new Uri("https://localhost/events/1"));
+    }
+
+    [Benchmark]
+    public void QueryString()
+    {
+      _templatedUriResolver.Match(new Uri("https://localhost/events/?by=gracekelly"));
+    }
+  }
+}
