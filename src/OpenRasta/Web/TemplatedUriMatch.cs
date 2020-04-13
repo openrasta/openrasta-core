356d6b5cf6a09025f27c81f309a335a60a82499f
Sebastien Lambla <seb@serialseb.com>
2020-04-14 00:20:35 +0200

    Restore properties (maybe)

--- src/OpenRasta/Web/TemplatedUriMatch.cs
+++ src/OpenRasta/Web/TemplatedUriMatch.cs
@@ -4,10 +4,14 @@ namespace OpenRasta.Web
 {
   public class TemplatedUriMatch
   {
+    public ResourceModel ResourceModel { get; }
+    public UriModel UriModel { get; }
     public UriTemplateMatch Match { get; }
 
     public TemplatedUriMatch(ResourceModel resourceModel, UriModel uriModel, UriTemplateMatch match)
     {
+      ResourceModel = resourceModel;
+      UriModel = uriModel;
       Match = match;
     }
   }
