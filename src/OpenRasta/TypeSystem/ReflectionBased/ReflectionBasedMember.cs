7c232230976db0077991137b93a8ff58f67162f8
Sebastien Lambla <seb@serialseb.com>
2020-04-19 12:42:12 +0200

    zelolz

--- src/OpenRasta/TypeSystem/ReflectionBased/ReflectionBasedMember.cs
+++ src/OpenRasta/TypeSystem/ReflectionBased/ReflectionBasedMember.cs
@@ -6,137 +6,143 @@ using System.Threading;
 
 namespace OpenRasta.TypeSystem.ReflectionBased
 {
-    public abstract class ReflectionBasedMember<T> : IMember
-        where T : IMemberBuilder
+  public abstract class ReflectionBasedMember<T> : IMember
+    where T : IMemberBuilder
+  {
+    readonly Dictionary<string, IProperty> _propertiesCachedByPath =
+      new Dictionary<string, IProperty>(StringComparer.OrdinalIgnoreCase);
+
+    readonly object _syncRoot = new object();
+
+    IType _memberType;
+
+    ILookup<string, IMethod> _methodsCache;
+
+    protected ReflectionBasedMember(ITypeSystem typeSystem, Type targetType)
     {
-        readonly Dictionary<string, IProperty> _propertiesCachedByPath =
-            new Dictionary<string, IProperty>(StringComparer.OrdinalIgnoreCase);
-
-        readonly object _syncRoot = new object();
-
-        IType _memberType;
-
-        ILookup<string, IMethod> _methodsCache;
-
-        protected ReflectionBasedMember(ITypeSystem typeSystem, Type targetType)
-        {
-            TypeSystem = typeSystem;
-            SurrogateProvider = typeSystem.SurrogateProvider;
-            PathManager = typeSystem.PathManager;
-            TargetType = targetType;
-        }
-
-        public virtual bool IsEnumerable => TargetType.IsArray || (TargetType.Implements(typeof(IEnumerable<>)) && !TargetType.Implements(typeof(IDictionary<,>)));
-
-        public virtual string Name => TargetType.Name;
-
-        public Type StaticType => TargetType;
-
-        public IPathManager PathManager { get; }
-
-        public ISurrogateProvider SurrogateProvider { get; }
-
-        public Type TargetType { get; }
-
-        public virtual IType Type
-        {
-            get
-            {
-              if (_memberType != null) return _memberType;
-              lock (_syncRoot)
-              {
-                Thread.MemoryBarrier();
-                _memberType ??= TypeSystem.FromClr(TargetType);
-              }
-
-              return _memberType;
-            }
-        }
-
-        public virtual string TypeName => TargetType.Name;
-
-        public ITypeSystem TypeSystem { get; set; }
-
-        public TAttribute FindAttribute<TAttribute>() where TAttribute : class
-        {
-            return FindAttributes<TAttribute>().FirstOrDefault();
-        }
-
-        public virtual IEnumerable<TAttribute> FindAttributes<TAttribute>() where TAttribute : class
-        {
-            return Attribute.GetCustomAttributes(TargetType, true).OfType<TAttribute>();
-        }
-
-        public virtual bool CanSetValue(object value)
-        {
-            return
-                TargetType.IsValueType && value != null && TargetType.IsInstanceOfType(value)
-                || TargetType.IsValueType && (value == null || TargetType.IsInstanceOfType(value));
-        }
-
-        public virtual IProperty GetIndexer(string indexerParameter)
-        {
-            var indexer = TargetType.FindIndexers(1).FindIndexer(indexerParameter);
-
-            return indexer != null ? SurrogateProperty(new ReflectionBasedProperty(TypeSystem, this, indexer.Value.Key, indexer.Value.Value)) : null;
-        }
-
-        public IMethod GetMethod(string methodName)
-        {
-            VerifyMethodsInitialized();
-
-            return _methodsCache.Contains(methodName) ? _methodsCache[methodName].FirstOrDefault() : null;
-        }
-
-        public IList<IMethod> GetMethods()
-        {
-            VerifyMethodsInitialized();
-            return _methodsCache.SelectMany(x => x).ToList().AsReadOnly();
-        }
-
-        public virtual IProperty GetProperty(string propertyName)
-        {
-            lock (_syncRoot)
-            {
-                if (_propertiesCachedByPath.ContainsKey(propertyName))
-                    return _propertiesCachedByPath[propertyName];
-                var pi = TargetType.FindPropertyCaseInvariant(propertyName);
-                if (pi == null)
-                    return null;
-
-                var pa = SurrogateProperty(new ReflectionBasedProperty(TypeSystem, this, pi, null));
-                _propertiesCachedByPath.Add(propertyName, pa);
-                return pa;
-            }
-        }
-
-        IProperty SurrogateProperty(IProperty property)
-        {
-            if (TypeSystem.SurrogateProvider == null) return property;
-            return TypeSystem.SurrogateProvider.FindSurrogate(property);
-        }
-
-        void VerifyMethodsInitialized()
-        {
-            if (_methodsCache == null)
-            {
-                lock (_syncRoot)
-                {
-                    Thread.MemoryBarrier();
-                    if (_methodsCache == null)
-                    {
-                        var allProperties = TargetType.GetProperties(BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static | BindingFlags.FlattenHierarchy);
-                        _methodsCache = (from method in TargetType.GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static | BindingFlags.FlattenHierarchy)
-                                         where !allProperties.Any(x => x.GetGetMethod() == method || x.GetSetMethod() == method)
-                                         select new ReflectionBasedMethod(
-                                           method.DeclaringType == TargetType
-                                             ? (IMember)this
-                                             : TypeSystem.FromClr(method.DeclaringType),
-                                           method) as IMethod)
-                            .ToLookup(x => x.Name, StringComparer.OrdinalIgnoreCase);
-                    }
-                }
-            }
-        }
+      TypeSystem = typeSystem;
+      SurrogateProvider = typeSystem.SurrogateProvider;
+      PathManager = typeSystem.PathManager;
+      TargetType = targetType;
     }
+
+    public virtual bool IsEnumerable => TargetType.IsArray ||
+                                        (TargetType.Implements(typeof(IEnumerable<>)) &&
+                                         !TargetType.Implements(typeof(IDictionary<,>)));
+
+    public virtual string Name => TargetType.Name;
+
+    public Type StaticType => TargetType;
+
+    public IPathManager PathManager { get; }
+
+    public ISurrogateProvider SurrogateProvider { get; }
+
+    public Type TargetType { get; }
+
+    public virtual IType Type
+    {
+      get
+      {
+        if (_memberType != null) return _memberType;
+        lock (_syncRoot)
+        {
+          Thread.MemoryBarrier();
+          _memberType ??= TypeSystem.FromClr(TargetType);
+        }
+
+        return _memberType;
+      }
+    }
+
+    public virtual string TypeName => TargetType.Name;
+
+    public ITypeSystem TypeSystem { get; set; }
+
+    public TAttribute FindAttribute<TAttribute>() where TAttribute : class
+    {
+      return FindAttributes<TAttribute>().FirstOrDefault();
+    }
+
+    public virtual IEnumerable<TAttribute> FindAttributes<TAttribute>() where TAttribute : class
+    {
+      return Attribute.GetCustomAttributes(TargetType, true).OfType<TAttribute>();
+    }
+
+    public virtual bool CanSetValue(object value)
+    {
+      return
+        TargetType.IsValueType && value != null && TargetType.IsInstanceOfType(value)
+        || !TargetType.IsValueType && (value == null || TargetType.IsInstanceOfType(value));
+    }
+
+    public virtual IProperty GetIndexer(string indexerParameter)
+    {
+      var indexer = TargetType.FindIndexers(1).FindIndexer(indexerParameter);
+
+      return indexer != null
+        ? SurrogateProperty(new ReflectionBasedProperty(TypeSystem, this, indexer.Value.Key, indexer.Value.Value))
+        : null;
+    }
+
+    public IMethod GetMethod(string methodName)
+    {
+      VerifyMethodsInitialized();
+
+      return _methodsCache.Contains(methodName) ? _methodsCache[methodName].FirstOrDefault() : null;
+    }
+
+    public IList<IMethod> GetMethods()
+    {
+      VerifyMethodsInitialized();
+      return _methodsCache.SelectMany(x => x).ToList().AsReadOnly();
+    }
+
+    public virtual IProperty GetProperty(string propertyName)
+    {
+      lock (_syncRoot)
+      {
+        if (_propertiesCachedByPath.ContainsKey(propertyName))
+          return _propertiesCachedByPath[propertyName];
+        var pi = TargetType.FindPropertyCaseInvariant(propertyName);
+        if (pi == null)
+          return null;
+
+        var pa = SurrogateProperty(new ReflectionBasedProperty(TypeSystem, this, pi, null));
+        _propertiesCachedByPath.Add(propertyName, pa);
+        return pa;
+      }
+    }
+
+    IProperty SurrogateProperty(IProperty property)
+    {
+      if (TypeSystem.SurrogateProvider == null) return property;
+      return TypeSystem.SurrogateProvider.FindSurrogate(property);
+    }
+
+    void VerifyMethodsInitialized()
+    {
+      if (_methodsCache == null)
+      {
+        lock (_syncRoot)
+        {
+          Thread.MemoryBarrier();
+          if (_methodsCache == null)
+          {
+            var allProperties = TargetType.GetProperties(BindingFlags.Public | BindingFlags.Instance |
+                                                         BindingFlags.Static | BindingFlags.FlattenHierarchy);
+            _methodsCache = (from method in TargetType.GetMethods(
+                  BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static | BindingFlags.FlattenHierarchy)
+                where !allProperties.Any(x => x.GetGetMethod() == method || x.GetSetMethod() == method)
+                select new ReflectionBasedMethod(
+                  method.DeclaringType == TargetType
+                    ? (IMember) this
+                    : TypeSystem.FromClr(method.DeclaringType),
+                  method) as IMethod)
+              .ToLookup(x => x.Name, StringComparer.OrdinalIgnoreCase);
+          }
+        }
+      }
+    }
+  }
 }
