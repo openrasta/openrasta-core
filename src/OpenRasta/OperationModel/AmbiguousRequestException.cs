63904906c332fefc8a0f3c9360229930ceb6333d
Sebastien Lambla <seb@serialseb.com>
2020-04-25 16:07:57 +0200

    Better diagnostics info

--- src/OpenRasta/OperationModel/AmbiguousRequestException.cs
+++ src/OpenRasta/OperationModel/AmbiguousRequestException.cs
@@ -9,8 +9,11 @@ namespace OpenRasta.OperationModel
     public AmbiguousRequestException(IEnumerable<IOperationAsync> operations)
     {
       Message = "The request has multiple operations it could match.\r\n" +
-                operations.Aggregate("", (str, operation) => str + $" - {operation.Name}\r\n");
+                operations.Aggregate("", (str, operation) => str + $" - {operation.Name}({RenderInputs(operation)})\r\n");
     }
+
+    static string RenderInputs(IOperationAsync operation) =>
+      string.Join(",", operation.Inputs.Select(input => input.Member.Name));
 
     public override string Message { get; }
   }
