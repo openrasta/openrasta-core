1ddf2c25669761185d7362806bcf3c76728d9bd4
Sebastien Lambla <seb@serialseb.com>
2020-04-25 16:10:56 +0200

    Add querystring scenario

--- src/Tests/Scenarios.HandlerSelection/CollectionHandler.cs
+++ src/Tests/Scenarios.HandlerSelection/CollectionHandler.cs
@@ -2,6 +2,14 @@
 {
   public class CollectionHandler<TId,TContent>
   {
+    public string Get()
+    {
+      return "GET";
+    }
+    public string Get(TId account_id)
+    {
+      return $"GET:{account_id}";
+    }
     public string Post(TId account_id, TContent content)
     {
       return "POST:" + account_id;
--- src/Tests/Scenarios.HandlerSelection/post_with_and_without_uri_var_as_string.cs
+++ src/Tests/Scenarios.HandlerSelection/post_with_and_without_uri_var_as_string.cs
@@ -35,4 +35,32 @@ namespace Tests.Scenarios.HandlerSelection
       response.ReadString().ShouldBe("POST");
     }
   }
+  public class get_with_and_without_query_string
+  {
+    InMemoryHost server;
+
+    public get_with_and_without_query_string()
+    {
+      server = new TestHost((has,uses) =>
+        has
+          .ResourcesNamed("example")
+          .AtUri("/account/")
+          .And.AtUri("/account/?id={account_id")
+          .HandledBy<CollectionHandler<string,string>>());
+    }
+
+    [Fact]
+    public async Task get_without_qs_gets_Selected()
+    {
+      var response = await server.Get("/account/");
+      response.ReadString().ShouldBe("GET");
+    }
+    
+    [Fact]
+    public async Task get_with_qs_gets_selected()
+    {
+      var response = await server.Get("/account/?id=1");
+      response.ReadString().ShouldBe("GET:1");
+    }
+  }
 }
